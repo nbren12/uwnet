861197e9aaac42d66523bcabd42471c5cd4fdd35
Noah D. Brenowitz <nbren12@gmail.com>
2017-08-22 15:06:54 -0700

    Put phsyical scales back in mca analysis

--- results/2017-08-12/mca.py
+++ results/2017-08-12/mca.py
@@ -23,9 +23,12 @@ rho = xr.open_dataset(snakemake.input.stat).RHO[-1]
 weight = np.sqrt(rho/integrate(rho, 'z'))
 
 # dependent variables
-q1 = xr.open_dataset(snakemake.input.q1).q1.pipe(mysel)
+q1 = xr.open_dataset(snakemake.input.q1).pipe(mysel)
 q2 = xr.open_dataset(snakemake.input.q2).q2.pipe(mysel)
-X, scale_X = xr2mat([q1, q2], ('time', 'x'), ('z',), weight=weight)
+
+q1c = q1.q1-q1.tend
+q1c.name = 'q1c'
+X, scale_X = xr2mat([q1c, q2], ('time', 'x'), ('z',), weight=weight)
 
 # independent variables
 sl = xr.open_dataset(snakemake.input.sl).sl.pipe(mysel)
@@ -42,8 +45,11 @@ Ux = svd2xr(U[:,:neig], X, weight, scale_X)
 Vx = svd2xr(Vt.T[:,:neig], Y, weight, scale_Y)
 S = xr.DataArray(S[:neig]**2, (np.r_[0:neig],), ('m',))/(S**2).sum()
 
-# output dataset
-d = xr.concat([Ux,Vx],'variable').to_dataset("variable")
-d['eig'] = S
+# rescale data
+d = xr.concat([Ux,Vx],'variable') * xr.concat([scale_X, scale_Y], 'variable')
+d = d/d.sel(variable='qt').max()
 
+# output dataset
+d = d.to_dataset('variable')
+d['eig'] = S
 d.to_netcdf(snakemake.output[0])
