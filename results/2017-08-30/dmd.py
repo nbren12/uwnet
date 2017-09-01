922b45b4d14c54f5f023da34a07fb98ca307b0a8
Noah D. Brenowitz <nbren12@gmail.com>
2017-09-01 12:51:29 -0700

    Add plotting routines and logm
    I also needed to change order of the features dimension in the prepared data

--- results/2017-08-30/dmd.py
+++ results/2017-08-30/dmd.py
@@ -10,10 +10,16 @@ from sklearn.linear_model import LinearRegression
 from gnl.xarray import xr2mat
 
 
+def logm(A):
+    w, v = np.linalg.eig(A)
+
+    return np.real(v@np.diag(np.log(w)) @ np.linalg.inv(v))
+
 class DMD(object):
     def __init__(self,  n_components=3, time_dim='time', feature_dims=None,
                  input_variables=None, output_variables=None,
                  weight_variable=None,
+                 scale_dim='z',
                  scale=True):
         self.n_components = n_components
         self.time_dim = time_dim
@@ -27,6 +33,7 @@ class DMD(object):
 
         self.weight_variable = weight_variable
         self.scale = scale
+        self.scale_dim = scale_dim
 
 
     def prepare_input(self, X):
@@ -35,23 +42,30 @@ class DMD(object):
             weight = X[self.weight_variable]
             Xw = X*np.sqrt(weight)
 
+        if self.feature_dims is None:
+            feats = ['variable'] + [x for x in X.dims if x != self.time_dim]
+
+        sample_dims = [x for x in X.dims if x not in feats]
+
+
+        # scales
+        if self.scale:
+            scale = Xw.std([self.scale_dim] + sample_dims)
+            Xw = Xw / scale
+
         X1 = Xw.isel(**{self.time_dim: slice(0, -1)})
         X2 = Xw.isel(**{self.time_dim: slice(1, None)})
 
-        if self.feature_dims is None:
-            feats = [x for x in X.dims if x != self.time_dim]\
-                + ['variable']
 
-        samples = [x for x in X.dims if x not in feats]
 
         Xd = X1[self.input_variables]\
              .to_array()\
-             .stack(features=feats, samples=samples)\
+             .stack(features=feats, samples=sample_dims)\
              .transpose('samples', 'features')
 
         Yd = X2[self.output_variables]\
              .to_array()\
-             .stack(features=feats, samples=samples)\
+             .stack(features=feats, samples=sample_dims)\
              .transpose('samples', 'features')
 
         return Xd, Yd
@@ -80,12 +94,7 @@ class DMD(object):
         P = self.svd_.components_
         At = self.lin_.coef_
 
-        w, v = np.linalg.eig(At)
-
-        lam = np.diag(np.log(w))
-        Bt = np.real(v @ lam @ np.linalg.inv(v))
-
-        return P.T @ Bt @ P
+        return P.T @ logm(At) @ P
 
 
 
@@ -115,28 +124,44 @@ def test_DMD():
     plt.show()
     assert False
 
+
+def plot_coef(A):
+    A1, A2 = np.split(A.T, 2, axis=0)
+    A11, A12 = np.split(A1, 2, axis=1)
+    A21, A22 = np.split(A2, 2, axis=1)
+
+    import matplotlib.pyplot as plt
+
+    fig, axs = plt.subplots(2,2, sharex=True, sharey=True)
+    for mat, ax in zip([A11, A12, A21, A22], axs.flat):
+        ax.pcolormesh(mat)
+
+    plt.show()
+
+
 def main():
     # D = xr.open_dataset("wd/A64/3d/QV.nc").isel(x=0)
-    D = xr.open_dataset("wd/A64/3d/TABS.nc").isel(x=0)
+    sl = xr.open_dataset("wd/calc/sl.nc").isel(x=0)
+    qt = xr.open_dataset("wd/calc/qt.nc").isel(x=0)
     stats = xr.open_dataset("wd/stat.nc")
     rho = stats.RHO[-1]
 
     # compute weight
-    D = xr.merge((D, rho))
+    D = xr.merge((sl, qt, rho))
 
 
-    dmd = DMD(time_dim='time', n_components=4,
-              input_variables=['TABS'],
+    dmd = DMD(time_dim='time', n_components=9,
+              input_variables=['qt', 'sl'],
               weight_variable='RHO')
     dmd.fit(D)
 
     y =  dmd.predict(D)
 
-    import matplotlib.pyplot as plt
 
     A = dmd.generator()
-    plt.pcolormesh(A)
-    plt.show()
+
+    plot_coef(A)
+    from IPython import embed; embed()
 
 if __name__ == '__main__':
     main()
