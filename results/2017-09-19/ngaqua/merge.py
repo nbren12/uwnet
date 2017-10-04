963a32a784222bdbc7497a0c62c7687f406e93cc
Noah D. Brenowitz <nbren12@gmail.com>
2017-10-04 14:38:52 -0700

    Remove ngaqua analysis from this folder

--- results/2017-09-19/ngaqua/merge.py
+++
@@ -1,8 +0,0 @@
-from snakemake.shell import shell
-
-input = snakemake.input
-output = snakemake.output
-
-shell("ncks {input[0]} {output}")
-for file in input[1:]:
-    shell("ncks -A {file} {output}")
