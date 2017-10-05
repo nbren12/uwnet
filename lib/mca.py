bd914c6da54d5ee1a58ea1dfafc84d90aae423fe
Noah D. Brenowitz <nbren12@gmail.com>
2017-10-05 11:02:45 -0700

    move library files to lib/

--- lib/mca.py
+++ lib/mca.py
@@ -1,60 +1,64 @@
-"""Perform maximum covariance analysis of q1, q2  vs sl, qt
+"""Code for maximum covariance analysis regression
 """
 import numpy as np
-from scipy.linalg import svd
-import pandas as pd
-import xarray as xr
-from gnl.xarray import integrate, xr2mat
+from scipy.linalg import svd, lstsq
+from sklearn.base import RegressorMixin, TransformerMixin, BaseEstimator
 
 
-def get_dz(z):
-    zext = np.hstack((0,  z,  2.0*z[-1] - 1.0*z[-2]))
-    zw = .5 * (zext[1:] + zext[:-1])
-    dz = zw[1:] - zw[:-1]
+class MCA(BaseEstimator, TransformerMixin):
 
-    return xr.DataArray(dz, z.coords)
+    def __init__(self, n_components=4, demean=True):
+        self.n_components = n_components
+        self.demean = demean
+
+    def fit(self, XY, y=None):
+
+        # This is basically a hack to work around the difficult
+        # with using scaled output for MCA but not for the linear regression
+        assert len(XY) == 2
+
+        X, Y = XY
+
+        X = np.asarray(X)
+        Y = np.asarray(Y)
+
+        m, n = X.shape
+        self.x_mean_ = X.mean(axis=0)
+        self.y_mean_ = Y.mean(axis=0)
+
+        if self.demean:
+            X = X - self.x_mean_
+            Y = Y - self.y_mean_
 
 
-def svd2xr(U, X, weight, scale):
-    neig = U.shape[1]
-    eig_idx = pd.Index(range(neig), name="m")
-    Ux = xr.DataArray(U, (X.features, eig_idx))\
-           .unstack('features')*scale/weight
-    return Ux
+        self.cov_ = X.T.dot(Y)/(m-1)  # covariance matrix
+        U, S, Vt = svd(self.cov_, full_matrices=False)
 
-def mysel(x):
-    return x.sel(time=slice(20,None))
+        self._u = U
+        self._v = Vt.T
 
-# density
-weight = xr.open_dataarray(snakemake.input.weight)
+        # x_scores = self.transform(X)
+        # b = lstsq(x_scores, Y)[0]
+        # self.coef_ = self.x_components_ @ b
 
-# dependent variables
-q1 = xr.open_dataset(snakemake.input.q1).pipe(mysel)
-q2 = xr.open_dataset(snakemake.input.q2).q2.pipe(mysel)
+        return self
 
-q1c = q1.q1-q1.tend
-q1c.name = 'q1c'
-X, scale_X = xr2mat([q1c, q2], ('time', 'x'), ('z',))
+    def transform(self, XY, y=None):
+        assert len(XY) == 2
+        X, Y = XY
+        X = np.asarray(X)
+        Y = np.asarray(Y)
+        x_scores = (X-self.x_mean_).dot(self.x_components_)
+        # if y is not None:
+        #     y_scores = (Y-self.y_mean_).dot(self.y_components_)
+        #     return x_scores, y_scores
+        # else:
+        return x_scores
 
-# independent variables
-sl = xr.open_dataset(snakemake.input.sl).sl.pipe(mysel)
-qt = xr.open_dataset(snakemake.input.qt).qt.pipe(mysel)
-Y, scale_Y = xr2mat([sl, qt], ('time', 'x'), ('z',))
+    @property
+    def x_components_(self):
+        return self._u[:, :self.n_components]
 
-# Perform the analysis
-C = X.values.T.dot(Y.values)/(len(X.samples)-1)  # covariance matrix
-U, S, Vt = svd(C, full_matrices=0)   # singular value decomposition
-
-# Recompose data
-neig = 20
-Ux = svd2xr(U[:,:neig], X, weight, scale_X)
-Vx = svd2xr(Vt.T[:,:neig], Y, weight, scale_Y)
-S = xr.DataArray(S[:neig]**2, (np.r_[0:neig],), ('m',))/(S**2).sum()
-
-# rescale data
-d = xr.concat([Ux,Vx],'variable') * xr.concat([scale_X, scale_Y], 'variable')
-
-# output dataset
-d = d.to_dataset('variable')
-d['eig'] = S
-d.to_netcdf(snakemake.output[0])
+    @property
+    def y_components_(self):
+        return self._v[:, :self.n_components]
