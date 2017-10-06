8742d06f3ed97863e26034e8a1c08338c6d27700
Noah D. Brenowitz <nbren12@gmail.com>
2017-10-05 18:17:14 -0700

    Started a new results/date folder for making pretty lrf plots
    TODO:
    
    1. Get a version of MCA that works nicely with Xarray Data. I suspect the MCA
    class should just perform its fitting on xarray objects directly since it seems
    like the analysis requires dimension aware behavior in too many places. The data
    can then be transformed using stack_cat.

--- lib/models.py
+++ lib/models.py
@@ -2,50 +2,61 @@ from functools import partial
 import pandas as pd
 import numpy as np
 import xarray as xr
-from sklearn.base import TransformerMixin, BaseEstimator
+from sklearn.base import TransformerMixin, BaseEstimator, RegressorMixin
 from sklearn.pipeline import make_pipeline
 from sklearn.linear_model import Ridge, LinearRegression
 from sklearn.decomposition import PCA
 from xnoah.data_matrix import stack_cat, unstack_cat
 from .mca import MCA
 
+
 def prepvar(X, feature_dims=['z'], sample_dims=['time', 'x', 'y']):
-   return  stack_cat(X, "features", ['z']).stack(samples=['time', 'x', 'y']).transpose("samples", "features")
+    return stack_cat(X, "features",
+                     ['z']).stack(samples=['time', 'x', 'y']).transpose(
+                         "samples", "features")
 
 
 def weights_to_np(w, feats):
     idx = feats.indexes['features']
+
     def f(i):
         if i < 0:
             return 1.0
         else:
             return float(w.sel(z=idx.levels[1][i]))
 
-    return xr.DataArray(np.array([f(i) for i in idx.labels[1]]), coords=(feats,))
+    return xr.DataArray(
+        np.array([f(i) for i in idx.labels[1]]), coords=(feats, ))
 
 
 class WeightedOutput(object):
     """Light weight class for weighting the output of an sklearn function"""
 
-    def __init__(self, model, w, nfit=10000):
+    def __init__(self, model=None, weight=None, nfit=10000):
         self._mod = model
-        self.w= np.asarray(w)
+        self.w = np.asarray(weight)
         self.nfit = nfit
 
     def fit(self, x, y):
         n = x.shape[0]
-        rand = np.random.choice(n, self.nfit)
-        self._mod.fit(x[rand], y[rand]*np.sqrt(self.w))
+        if self.nfit is None:
+            rand = slice(None)
+        else:
+            rand = np.random.choice(n, self.nfit)
+        self._mod.fit(x[rand], y[rand] * np.sqrt(self.w))
         return self
 
     def predict(self, x):
-        return self._mod.predict(x)/np.sqrt(self.w)
+        return self._mod.predict(x) / np.sqrt(self.w)
 
     def score(self, x, y):
         return self._mod.score(x, y * np.sqrt(self.w))
 
     def __repr__(self):
-        return "WeightedOutput(%s)"%repr(self._mod)
+        return "WeightedOutput(%s)" % repr(self._mod)
+
+    def __getattr__(self, key):
+        return getattr(self._mod, key)
 
     @classmethod
     def quickfit(cls, in_file, out_file, w_file, model=None, **kwargs):
@@ -65,17 +76,74 @@ class WeightedOutput(object):
 
         return dict(x=xmat, y=ymat, w=wmat, mod=mod.fit(xmat, ymat))
 
+def compute_dp(p):
+    p_ghosted = np.hstack((2 * p[0] - p[1], p, 0))
+    p_interface = (p_ghosted[1:] + p_ghosted[:-1]) / 2
+    dp = -np.diff(p_interface)
+
+    return dp
+
+
+def plot_lrf(lrf,
+             p,
+             input_vars,
+             output_vars,
+             width_ratios=[1, 1, .3, .3],
+             figsize=(10, 5),
+             image_kwargs={}):
+    import matplotlib.pyplot as plt
+    import matplotlib.gridspec as gridspec
+    p = np.asarray(p)
+    ni, no = len(input_vars), len(output_vars)
+    print("Making figure with", ni, "by", no, "panes")
+
+    fig, axs = plt.subplots(no, ni, figsize=figsize)
+
+    grid = gridspec.GridSpec(2, len(input_vars), width_ratios=width_ratios)
+
+    for j, input_var in enumerate(input_vars):
+        for i, output_var in enumerate(output_vars):
+            ax = plt.subplot(grid[i, j])
+            lrf_pane = np.asarray(lrf.loc[input_var][output_var])
+            #             print(lrf_pane.shape)
+            if lrf_pane.shape[0] == 1:
+                ax.plot(lrf_pane.ravel(), p)
+                ax.invert_yaxis()
+            else:
+                # compute pressure difference
+                dp = compute_dp(p)
+                im = ax.pcolormesh(p, p, (lrf_pane).T / dp, **image_kwargs)
+                # make colorbar
+                cbar = plt.colorbar(im, orientation='vertical', pad=.02)
+                # invert y axis for pressure coordinates
+                ax.invert_yaxis()
+                ax.invert_xaxis()
+
+            # turn off axes ylabels
+            if j > 0:
+                ax.set_yticks([])
+            if i < no - 1:
+                ax.set_xticks([])
+
+            # Add variable names
+            if j == 0:
+                ax.set_ylabel(output_var)
+
+            if i == 0:
+                ax.set_title(input_var)
+
+    return fig, axs
+
 
 def get_lrf(mod, xmat, ymat):
     m = len(xmat.indexes['features'])
-    lrf = mod.predict(np.eye(m)) - mod.predict(0*np.eye(m))
+    lrf = mod.predict(np.eye(m)) - mod.predict(0 * np.eye(m))
     row, col = xmat.indexes['features'], ymat.indexes['features']
-    return pd.DataFrame(lrf, index=row, columns=col) 
+    return pd.DataFrame(lrf, index=row, columns=col)
 
 
 def get_mat(ds, sample_dims=['x', 'time']):
-    feature_dims = [dim for dim in ds.dims
-                    if dim not in sample_dims]
+    feature_dims = [dim for dim in ds.dims if dim not in sample_dims]
 
     return stack_cat(ds, 'features', feature_dims) \
         .stack(samples=sample_dims) \
@@ -86,8 +154,9 @@ def compute_weighted_scale(weight, sample_dims, ds):
     def f(data):
         sig = data.std(sample_dims)
         if set(weight.dims) <= set(sig.dims):
-            sig = (sig ** 2 * weight).sum(weight.dims).pipe(np.sqrt)
+            sig = (sig**2 * weight).sum(weight.dims).pipe(np.sqrt)
         return sig
+
     return ds.apply(f)
 
 
@@ -97,11 +166,11 @@ def mul_if_dims_subset(weight, x):
     else:
         return x
 
-def  _prepare_variable(x, sample_dims, scale=None, weight=None):
+
+def _prepare_variable(x, sample_dims, scale=None, weight=None):
 
     if scale is not None:
         x /= scale
-
 
     if weight is not None:
         weighter = partial(mul_if_dims_subset, np.sqrt(weight))
@@ -110,7 +179,7 @@ def  _prepare_variable(x, sample_dims, scale=None, weight=None):
     return get_mat(x, sample_dims)
 
 
-def  _unprepare_variable(y, sample_dims, scale=None, weight=None):
+def _unprepare_variable(y, sample_dims, scale=None, weight=None):
 
     y = unstack_cat(y, 'features').unstack('samples')
 
@@ -118,7 +187,7 @@ def  _unprepare_variable(y, sample_dims, scale=None, weight=None):
         y *= scale
 
     if weight is not None:
-        weighter = partial(mul_if_dims_subset, 1/np.sqrt(weight))
+        weighter = partial(mul_if_dims_subset, 1 / np.sqrt(weight))
         y = y.apply(weighter)
 
     return y
@@ -131,7 +200,7 @@ def _unstack(y, coords):
 
 def _score_dataset(y_true, y, sample_dims, return_scalar=True):
 
-    if not set(y.dims)  >= set(sample_dims):
+    if not set(y.dims) >= set(sample_dims):
         raise ValueError("Sample dims must be a subset of data dimensions")
 
     # means
@@ -140,11 +209,11 @@ def _score_dataset(y_true, y, sample_dims, return_scalar=True):
     # prediction
     sse = ((y_true - y)**2).sum(sample_dims).sum()
 
-    r2 = 1- sse/ss
+    r2 = 1 - sse / ss
     sse_ = sse.to_array().sum()
     ss_ = ss.to_array().sum()
 
-    scores = {'total': float(1.0- sse_/ss_)}
+    scores = {'total': float(1.0 - sse_ / ss_)}
     for key in r2:
         scores[key] = float(r2[key])
 
@@ -159,23 +228,22 @@ class XWrapper(object):
         self.scales_x_ = None
 
     def fit(self, x, y):
-        self.scales_x_ = compute_weighted_scale(self.weight,
-                                                self.sample_dims,
+        self.scales_x_ = compute_weighted_scale(self.weight, self.sample_dims,
                                                 x)
         x, y = self.prepvars(x, y)
         self.yfeats = y.features
         self.xfeats = x.features
-        self._model.fit(x,y)
+        self._model.fit(x, y)
         return self
 
     def predict(self, x):
         x = self.prepvars(x)
-        y =  self._model.predict(x)
+        y = self._model.predict(x)
 
         coords = (x.samples, self.yfeats)
 
-        return _unprepare_variable(xr.DataArray(y, coords), self.sample_dims,
-                                   weight=self.weight)
+        return _unprepare_variable(
+            xr.DataArray(y, coords), self.sample_dims, weight=self.weight)
 
     def score(self, x, y):
         pred = self.predict(x)
@@ -204,12 +272,13 @@ class XWrapper(object):
 
 # Ridge Regression
 MyRidge = make_pipeline(Ridge(100.0, normalize=True))
-MyRidge.prep_kwargs = dict(scale_input=True, scale_output=False,
-                           weight_input=True, weight_output=True)
+MyRidge.prep_kwargs = dict(
+    scale_input=True,
+    scale_output=False,
+    weight_input=True,
+    weight_output=True)
 MyRidge.param_grid = {'ridge__alpha': np.logspace(-10, 3, 15)}
 # MyRidge.param_grid = {'ridge__alpha': [.19]}
 
 # This dictionary is used by the Snakefile
-model_dict = {
-    'ridge': MyRidge
-}
+model_dict = {'ridge': MyRidge}
