dc2a1fc4a1da80301760d58d865c0d58f9cf46da
Noah D. Brenowitz <nbren12@gmail.com>
2017-10-11 17:04:43 -0700

    Compute linear response function in the linear_regression script

--- lib/linear_regression.py
+++ lib/linear_regression.py
@@ -14,15 +14,23 @@ data = joblib.load(snakemake.input[0])
 x_train, y_train = data['train']
 x_test, y_test = data['test']
 
+scale_in, scale_out = data['scale']
+
 
 mod = make_pipeline(VarianceThreshold(.001), LinearRegression())
-mod.fit(x_train, y_train)
+mod.fit(x_train/scale_in, y_train)
 score = mod.score(x_test, y_test)
+
+# compute matrix
+I = np.eye(x_train.shape[1])
+mat = mod.predict(I) - mod.predict(I * 0)
+mat = np.diag(1/scale_in) @ mat
 
 output_data = {
     'condition_number': np.linalg.cond(x_train),
     'test_score': score,
     'model': mod,
+    'mat': mat,
     'features': {'in': x_test.indexes['features'],
                  'out': y_test.indexes['features']}}
 
