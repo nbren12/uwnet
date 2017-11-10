f763419259b51299b8dd788c54168fad9bdfc2d0
Noah D. Brenowitz <nbren12@gmail.com>
2017-11-10 11:48:48 -0800

    Add ndoverlay_to_contours to hvops.py
    This function as contributed by Phillip Rudiger on the gitter channel.

--- lib/hvops.py
+++ lib/hvops.py
@@ -42,3 +42,31 @@ def shade_points(scat):
         .redim.range(true=(0,1), lm=(0,1)) \
         .redim.unit(lm="%", true="%")\
         * hv.Curve(((0,1),(0,1))).opts(style=dict(color='black'))
+
+
+def ndoverlay_to_contours(ndoverlay):
+    """Turn an NDOverlay into a contour
+
+    Notes
+    -----
+    Provided by Phillip Rudiger on the gitter channel
+
+    Examples
+    --------
+    >>>     def ndoverlay_to_contours(ndoverlay):
+    ...         dims = ndoverlay.dimensions(label='name')
+    ...         return hv.Contours([dict(v.columns(), **dict(zip(dims, k))) for k, v in ndoverlay.data.items()],
+    ...                         kdims=ndoverlay.last.dimensions()[:2], vdims=ndoverlay.kdims)
+    ...
+    ...     curves = hv.HoloMap({(chr(n+65), n): hv.Curve(np.random.rand(10)) for n in range(10)}, kdims=['variable', 'n'])
+    ...     curves.overlay('n').grid('variable').map(ndoverlay_to_contours, hv.NdOverlay)
+    ...
+    """
+    dims = ndoverlay.dimensions(label='name')
+    return hv.Contours(
+        [
+            dict(v.columns(), **dict(zip(dims, k)))
+            for k, v in ndoverlay.data.items()
+        ],
+        kdims=ndoverlay.last.dimensions()[:2],
+        vdims=ndoverlay.kdims)
