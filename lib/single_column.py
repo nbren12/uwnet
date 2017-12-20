7e2439a7a07eb371a5e23b78bb0755099a51aa54
Noah D. Brenowitz <nbren12@gmail.com>
2017-12-20 13:25:59 -0800

    Refactor single column simulations
    I moved the runsteps, and forced step functions to lib.single_column.py

---
+++ lib/single_column.py
@@ -0,0 +1,31 @@
+import numpy as np
+import torch
+from toolz import curry
+from .models.torch_models import predict
+
+def runsteps(step, x, n):
+    """Perform n steps using"""
+    out = np.zeros((n+1, x.shape[0]), dtype=x.dtype)
+    out[0] = x
+
+    for i in range(n):
+        x = step(x, i)
+        out[i] = x
+
+    return out
+
+
+@curry
+def step(net, x, n):
+    return predict(net, x[None,:])[0,:]
+
+
+@curry
+def forced_step(net, g, x, n):
+
+    if n < g.shape[0]-1:
+        f = (g[n] + g[n+1])/2
+    else:
+        f = g[n]
+
+    return predict(net, x[None,:])[0,:] + f * 3/24
