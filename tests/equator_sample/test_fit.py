68d0bd32911f29958ed8b3b1ae07088ffaa16006
Noah D. Brenowitz <nbren12@gmail.com>
2017-12-22 00:09:18 -0800

    test_fit runs succesfully

--- tests/equator_sample/test_fit.py
+++ tests/equator_sample/test_fit.py
@@ -1,13 +1,13 @@
-from lib.preprocess import ForcedData
-from lib.models.multiple_step_objective import train_multistep_objective
+from lib.models.torch.preprocess import prepare_data
+from lib.models.torch.multiple_step_objective import train_multistep_objective
 import torch
 
 
 
-data = ForcedData.from_files("data/prog/*.nc", "data/forcing/*.nc",
-                             "data/w.nc")
-
+data = prepare_data("data/prog/*.nc", "data/forcing/*.nc", "data/w.nc",
+                    subset_fn=lambda x: x)
 net = train_multistep_objective(data, num_epochs=2, weight_decay=.1,
+                                window_size=10,
                                 learning_rate=.005)
 
 torch.save(net, "net.torch")
