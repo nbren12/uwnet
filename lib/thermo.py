d592a4a17b0157907583a538ed8903557a1fd710
Noah D. Brenowitz <nbren12@gmail.com>
2018-01-12 16:42:56 -0800

    Analyze the predicted precipitation of the NN scheme
    To do this, I had to add some functions to lib.thermo. These functions
    include mass_weighted averaging and a nice decorator which can be used
    to wrap metpy functions for use with xarray data.

--- lib/thermo.py
+++ lib/thermo.py
@@ -54,9 +54,33 @@ def get_dz(z):
 
     return xr.DataArray(dz, z.coords)
 
+
 def layer_mass(rho):
     dz = get_dz(rho.z)
     return rho*dz
+
+
+def layer_mass_from_p(p, ps=None):
+    if ps is None:
+        ps = 2*p[0] - p[1]
+
+    ptop = p[-1]*2 - p[-2]
+
+    pext = np.hstack((ps, p, ptop))
+    pint = (pext[1:] + pext[:-1])/2
+    dp = - np.diff(pint*100)/grav
+
+    return xr.DataArray(dp, p.coords)
+
+
+def mass_integrate(p, x, average=False):
+    dp = layer_mass_from_p(p)
+    ans = (x * dp).sum(p.dims)
+
+    if average:
+        ans = ans / dp.sum()
+
+    return ans
 
 
 # Rules for Linking data
@@ -72,4 +96,30 @@ def link_files(files):
         path = Path(dest)
         path.parent.mkdir(exist_ok=True)
         os.system(f"ln -s {src} {path}")
+
+def metpy_wrapper(fun):
+    """Given a metpy function return an xarray compatible version
+    """
+    from metpy.units import units as u
+
+    def func(*args):
+        def f(*largs):
+            new_args = [u.Quantity(larg, arg.units)
+                        for larg, arg in zip(largs, args)]
+            return fun(*new_args)
+
+        output_units = f(*[1 for arg in args]).units
+        ds = xr.apply_ufunc(f, *args)
+        ds.attrs['units'] = str(output_units)
+        return ds
+
+    return func
+
+
+def column_rh(QV, TABS, p):
+    from metpy.calc import relative_humidity_from_mixing_ratio
+    rh = metpy_wrapper(relative_humidity_from_mixing_ratio)(QV, TABS, p)
+
+    return mass_integrate(p, rh/1000, average=True)
+
 
