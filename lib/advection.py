7326aec103a2e1424b3b5ec56fbc4b84d6917cd1
Noah D. Brenowitz <nbren12@gmail.com>
2018-01-18 15:35:23 -0800

    add a lib.advection module for computing material derivatives

---
+++ lib/advection.py
@@ -0,0 +1,24 @@
+import numpy as np
+import xarray as xr
+from xnoah.xcalc import centderiv
+from xnoah.sam.coarsen import destagger
+
+
+
+def material_derivative(u, v, w, f):
+
+    wc = destagger(w, 'z', mode='clip')
+
+    # compute total derivative
+    df = u * centderiv(f, dim='x', boundary='periodic')\
+        + v * centderiv(f, dim='y', boundary='nearest')\
+        + wc * centderiv(f, dim='z', boundary='nearest')
+
+    try:
+        df.attrs['units'] = f.units.strip() + '/s'
+    except AttributeError:
+        pass
+
+
+    return df
+
