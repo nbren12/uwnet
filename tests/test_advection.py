57fea44eb937fc6803d1724721b1a87e60e08c3a
Noah D. Brenowitz <nbren12@gmail.com>
2018-01-18 17:27:23 -0800

    add test of material derivative

---
+++ tests/test_advection.py
@@ -0,0 +1,31 @@
+import numpy as np
+import xarray as xr
+from lib.advection import material_derivative
+
+def test_material_derivative():
+
+    dims = ['x', 'y', 'z', 'time']
+    coords = {dim: np.arange(10) for dim in dims}
+    shape = [coords[dim].shape[0] for dim in coords]
+
+    f = xr.Dataset({'f': (dims, np.ones(shape))}, coords=coords)
+    f = f.f
+
+    one = 0 *f +1
+    zero = 0*f
+
+
+    md = material_derivative(zero, one, zero, f.x + 0*f)
+    np.testing.assert_array_almost_equal(md.values, 0)
+
+    md = material_derivative(one, zero, zero, f.x + 0*f)
+    np.testing.assert_array_almost_equal(md.isel(x=slice(1,-1)).values,
+                                         one.isel(x=slice(1,-1)).values)
+
+    md = material_derivative(zero, one, zero, f.y + 0*f)
+    np.testing.assert_array_almost_equal(md.isel(y=slice(1,-1)).values,
+                                         one.isel(y=slice(1,-1)).values)
+
+    md = material_derivative(zero, zero, one, f.z + 0*f)
+    np.testing.assert_array_almost_equal(md.isel(z=slice(1,-1)).values,
+                                         one.isel(z=slice(1,-1)).values)
