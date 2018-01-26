65fddf5966e02c2eb5d4e9f1ad96a15250170fb1
Noah D. Brenowitz <nbren12@gmail.com>
2018-01-26 13:23:23 -0800

    add regression tests, but skip them in tests
    These regression tests might be useful later, but now that my refactor
    is done. I can skip them.

--- tests/test_regression_dict_input.py
+++ tests/test_regression_dict_input.py
@@ -29,20 +29,24 @@ def train_data():
     return joblib.load("data/ml/ngaqua/time_series_data.pkl")
 
 
-# def test_train_multistep_objective(train_data, test_data, regtest):
-def test_train_multistep_objective(train_data, test_data):
+@pytest.mark.skip()
+def test_train_multistep_objective(train_data, test_data, regtest):
 
-
-    train_data = {key: _stacked_to_dict(val) for key, val in train_data.items()
-                  if key != 'p'}
-    model = train_multistep_objective(train_data)
+    # train_data = {key: _stacked_to_dict(val) for key, val in train_data.items()
+    #               if key != 'p'}
+    model, _ = train_multistep_objective(train_data)
     wrapped = wrap(model)
 
 
     inputs, forcings = test_data
+    inputs = inputs.drop('p').drop('w')
     output = wrapped(inputs, forcings)
+    print(output.isel(time=-1), file=regtest)
 
-    # print(output.isel(time=-1), file=regtest)
+@pytest.mark.skip()
+def test_train_loss(train_data, regtest):
 
-
-
+    # train_data = {key: _stacked_to_dict(val) for key, val in train_data.items()
+    #               if key != 'p'}
+    _, loss = train_multistep_objective(train_data)
+    print(loss, file=regtest)
