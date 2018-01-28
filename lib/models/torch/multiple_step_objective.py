e70717b37dfe3d9e783e9572274db53a24904c3e
Noah D. Brenowitz <nbren12@gmail.com>
2018-01-28 09:38:39 -0800

    clean up source code in multiple_step_objective
    I moved the functions to the top of the module and ran yapf

--- lib/models/torch/multiple_step_objective.py
+++ lib/models/torch/multiple_step_objective.py
@@ -20,18 +20,12 @@ def _numpy_to_variable(x):
 def _data_to_torch_dataset(data, window_size):
 
     X, G = data['X'], data['G']
-    X = {key: WindowedData(val, window_size)
-         for key, val in X.items()}
-    G = {key: WindowedData(val, window_size)
-         for key, val in G.items()}
+    X = {key: WindowedData(val, window_size) for key, val in X.items()}
+    G = {key: WindowedData(val, window_size) for key, val in G.items()}
 
-    dataset = ConcatDataset(X['sl'], X['qt'], G['sl'], G['qt'],
-                            G['LHF'], G['SHF'])
+    dataset = ConcatDataset(X['sl'], X['qt'], G['sl'], G['qt'], G['LHF'],
+                            G['SHF'])
     return dataset
-
-
-def _stack_qtsl(X):
-    return np.concatenate((X['sl'], X['qt']), axis=-1)
 
 
 def _data_to_scaler(data, cuda=False):
@@ -60,7 +54,7 @@ def _data_to_scaler(data, cuda=False):
 
 
 def _to_dict(x):
-    return {'sl': x[...,:34], 'qt': x[...,34:]}
+    return {'sl': x[..., :34], 'qt': x[..., 34:]}
 
 
 def _from_dict(d):
@@ -68,10 +62,10 @@ def _from_dict(d):
 
 
 def _data_to_loss_feature_weights(data, cuda=True):
-
     def _f(args):
         w, scale = args
-        return w/scale**2
+        return w / scale**2
+
     w = merge_with(_f, data['w'], data['scales'])
     w = valmap(_numpy_to_variable, w)
 
@@ -80,69 +74,13 @@ def _data_to_loss_feature_weights(data, cuda=True):
 
     return w
 
-@curry
-def weighted_loss(weight, x, y):
-    return torch.mean(torch.pow(x-y, 2).mul(weight.float()))
-
 
 def _euler_step(prog, src, h):
     for key in prog:
         x = prog[key]
         f = src[key]
-        prog = assoc(prog, key, x + h*f)
+        prog = assoc(prog, key, x + h * f)
     return prog
-
-
-class ForcedStepper(nn.Module):
-    def __init__(self, rhs, h, nsteps):
-        super(ForcedStepper, self).__init__()
-        self.nsteps = nsteps
-        self.h = h
-        self.rhs = rhs
-
-    def forward(self, x, g):
-        """
-
-        Parameters
-        ----------
-        x : (seq_len, batch, input_size)
-            tensor containing prognostic variables
-        g : (seq_len, batch, input_size)
-            tensor containing forcings
-        """
-        window_size = first(x.values()).size(0)
-        d = valmap(lambda x: x[0], x)
-        g_dict = valmap(lambda x: (x[1:] + x[:-1])/2,  g)
-
-        h = self.h
-        nsteps = self.nsteps
-
-        # output array
-        steps = {key: [d[key]] for key in d}
-
-        for i in range(1, window_size):
-            for j in range(nsteps):
-                src = self.rhs(d)
-                large_scale_forcing = {key: val[i-1] for key, val in g_dict.items()}
-                d = _euler_step(d, src, h/nsteps)
-                d = _euler_step(d, large_scale_forcing, h/nsteps)
-
-            # store data
-            for key in d:
-                steps[key].append(d[key])
-
-        return valmap(torch.stack, steps)
-
-
-def mlp(layer_sizes):
-    layers = []
-    n = len(layer_sizes)
-    for k in range(n-1):
-        layers.append(nn.Linear(layer_sizes[k], layer_sizes[k+1]))
-        if k < n - 2:
-            layers.append(nn.ReLU())
-
-    return nn.Sequential(*layers)
 
 
 def _scale_var(scale, mean, x):
@@ -166,15 +104,72 @@ def scaler(scales, means, x):
             out[key] = x[key]
     return out
 
+
+@curry
+def weighted_loss(weight, x, y):
+    return torch.mean(torch.pow(x - y, 2).mul(weight.float()))
+
+
+def mlp(layer_sizes):
+    layers = []
+    n = len(layer_sizes)
+    for k in range(n - 1):
+        layers.append(nn.Linear(layer_sizes[k], layer_sizes[k + 1]))
+        if k < n - 2:
+            layers.append(nn.ReLU())
+
+    return nn.Sequential(*layers)
+
+
+class ForcedStepper(nn.Module):
+    def __init__(self, rhs, h, nsteps):
+        super(ForcedStepper, self).__init__()
+        self.nsteps = nsteps
+        self.h = h
+        self.rhs = rhs
+
+    def forward(self, x, g):
+        """
+
+        Parameters
+        ----------
+        x : (seq_len, batch, input_size)
+            tensor containing prognostic variables
+        g : (seq_len, batch, input_size)
+            tensor containing forcings
+        """
+        window_size = first(x.values()).size(0)
+        d = valmap(lambda x: x[0], x)
+        g_dict = valmap(lambda x: (x[1:] + x[:-1]) / 2, g)
+
+        h = self.h
+        nsteps = self.nsteps
+
+        # output array
+        steps = {key: [d[key]] for key in d}
+
+        for i in range(1, window_size):
+            for j in range(nsteps):
+                src = self.rhs(d)
+                large_scale_forcing = {
+                    key: val[i - 1]
+                    for key, val in g_dict.items()
+                }
+                d = _euler_step(d, src, h / nsteps)
+                d = _euler_step(d, large_scale_forcing, h / nsteps)
+
+            # store data
+            for key in d:
+                steps[key].append(d[key])
+
+        return valmap(torch.stack, steps)
+
+
 class RHS(nn.Module):
     def __init__(self, m, hidden=(), scaler=None):
         super(RHS, self).__init__()
-        self.mlp = mlp((m,) + hidden + (m,))
+        self.mlp = mlp((m, ) + hidden + (m, ))
         self.lin = nn.Linear(m, m, bias=False)
-
-        mask = torch.ones(m)
-        mask[-14:] = 0
-        self.mask = Variable(mask, requires_grad=False)
         self.scaler = scaler
 
     def forward(self, x):
@@ -186,9 +181,15 @@ class RHS(nn.Module):
         return _to_dict(y)
 
 
-def train_multistep_objective(data, num_epochs=4, window_size=10,
-                              num_steps=500, batch_size=100, lr=0.01,
-                              weight_decay=0.0, nsteps=1, nhidden=(10, 10, 10),
+def train_multistep_objective(data,
+                              num_epochs=4,
+                              window_size=10,
+                              num_steps=500,
+                              batch_size=100,
+                              lr=0.01,
+                              weight_decay=0.0,
+                              nsteps=1,
+                              nhidden=(10, 10, 10),
                               cuda=False,
                               test_loss=False):
     """Train a single layer perceptron euler time stepping model
@@ -214,15 +215,13 @@ def train_multistep_objective(data, num_epochs=4, window_size=10,
     scaler = _data_to_scaler(data, cuda=cuda)
     weights = _data_to_loss_feature_weights(data, cuda=cuda)
 
-
     # define the neural network
     m = sum(valmap(lambda x: x.size(-1), weights).values())
 
-
     rhs = RHS(m, hidden=nhidden, scaler=scaler)
     nstepper = ForcedStepper(rhs, h=dt, nsteps=nsteps)
-    optimizer = torch.optim.Adam(rhs.parameters(), lr=lr,
-                                 weight_decay=weight_decay)
+    optimizer = torch.optim.Adam(
+        rhs.parameters(), lr=lr, weight_decay=weight_decay)
 
     if cuda:
         nstepper.cuda()
@@ -230,13 +229,12 @@ def train_multistep_objective(data, num_epochs=4, window_size=10,
     def loss(y, x):
         total_loss = 0
         for key in y:
-            total_loss += weighted_loss(weights[key], x[key], y[key])/len(y)
+            total_loss += weighted_loss(weights[key], x[key], y[key]) / len(y)
         return total_loss
 
     # _init_linear_weights(net, .01/nsteps)
     def closure(*args):
-        args = [Variable(x.float()).transpose(0, 1)
-                for x in args]
+        args = [Variable(x.float()).transpose(0, 1) for x in args]
 
         if cuda:
             args = [arg.cuda() for arg in args]
@@ -253,10 +251,10 @@ def train_multistep_objective(data, num_epochs=4, window_size=10,
         args = next(iter(data_loader))
         return nstepper, closure(*args)
     else:
-        train(data_loader, closure, optimizer=optimizer,
-              num_epochs=num_epochs, num_steps=num_steps)
+        train(
+            data_loader,
+            closure,
+            optimizer=optimizer,
+            num_epochs=num_epochs,
+            num_steps=num_steps)
         return nstepper
-
-
-
-
