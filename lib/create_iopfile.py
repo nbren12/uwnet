db35fd926b2d028375d38ea0f65d34779bfd131b
Noah D. Brenowitz <nbren12@gmail.com>
2018-02-05 14:13:42 -0800

    Add script to create iop files for SCAM

--- lib/create_iopfile.py
+++ lib/create_iopfile.py
@@ -230,3 +230,153 @@ variables:
 		:date = "10-Aug-2006" ;
 
 """
+import numpy as np
+import xarray as xr
+
+from xnoah import swap_coord
+from .thermo import omega_from_w
+from .advection import vertical_advection, horizontal_advection
+
+
+circumference_earth = 4.0075e7
+
+
+def open_and_merge(file_2d, files_3d, stat_file):
+    data_3d = xr.open_mfdataset(files_3d, preprocess=lambda x: x.drop('p'))
+    data_2d = xr.open_dataset(file_2d)
+    data_2d = data_2d.isel(time=np.argsort(data_2d.time.values))
+    stat = xr.open_dataset(stat_file)
+    return xr.merge((data_3d, data_2d, stat.p, stat.RHO, stat.Ps),
+                    join='inner')
+
+
+def compute_tendencies(data):
+    return dict(
+        divT=xr.DataArray(
+            -horizontal_advection(data.U, data.V, data.TABS),
+            attrs={'units': 'K/s'}),
+        divq=xr.DataArray(
+            -horizontal_advection(data.U, data.V, data.QV/1000),
+            attrs={'units': 'kg/kg/s'}),
+        vertdivT=xr.DataArray(
+            -vertical_advection(data.W, data.TABS),
+            attrs={'units': 'K/s'}),
+        vertdivq=xr.DataArray(
+            -vertical_advection(data.W, data.QV/1000),
+            attrs={'units': 'kg/kg/s'}),
+    )
+
+
+def x_to_lon(x):
+    return xr.DataArray(x/circumference_earth*360,
+                        attrs={'units': 'deg E',
+                               'long_name': 'longitude'})
+
+
+def y_to_lat(y):
+    n = len(y)
+    if n % 2 == 1:
+        raise ValueError("Y coordinate must have even number of points")
+    else:
+        n2 = n//2
+    ymid = (y[n2-1] + y[n2])/2
+    lat = (y-ymid)/circumference_earth*360
+    return xr.DataArray(lat,
+                        attrs={'units': 'deg N',
+                               'long_name': 'latitude'})
+
+vars_3d = ['q', 'T', 'u', 'v', 'divT', 'divq', 'vertdivT', 'vertdivq']
+vars_2d = ['shflk', 'lhflx', 'Ptend', 'Ps', 'phis']
+
+def expand_dims(x):
+    if x.name in vars_3d + vars_2d:
+        return x.expand_dims(['lat', 'lon'])
+    else:
+        return x
+
+
+def prepare_iop_dataset(data):
+    data_vars = dict(
+        # time related variables
+        bdate=xr.DataArray(990101, attrs={'units': "yymmdd"}),
+        tsec=xr.DataArray(data.time*86400, attrs={'units': "s"}),
+        # spatial coordinates
+        lev=xr.DataArray(data.p*100, attrs={'units': 'Pa'}),
+        lat=y_to_lat(data.y),
+        lon=x_to_lon(data.x),
+        # surface variables
+        phis=0.0,
+        Ps=xr.DataArray(data.Ps*100, attrs={'units': 'Pa'}),
+        Ptend=data.Ps * 0,
+        shflx=data.SHF,
+        lhflx=data.LHF,
+        # 3d variables
+        q=xr.DataArray(data.QV/1000, attrs={'units': 'kg/kg'}),
+        T=data.TABS,
+        u=data.U,
+        v=data.V,
+        omega=xr.DataArray(omega_from_w(data.W, data.RHO[-1]),
+                           attrs={'units': 'Pa/s'}),
+    )
+
+    data_vars.update(compute_tendencies(data))
+
+    ds = xr.Dataset(data_vars)
+    ds = (swap_coord(ds, {"time": "tsec", 'x': 'lon', 'y': 'lat', 'z': 'lev'})
+          .transpose('tsec', 'lev', 'lat', 'lon')
+          .sortby('lev'))
+
+    return ds.drop('time')
+
+
+namelist_template = """
+&atm
+    iopfile='iop.nc'
+    nhtfrq=-1
+    single_column=.true.
+    scmlat= {lat:.4f}
+    scmlon= {lon:.4f}
+    aqua_planet = .true.
+    orb_obliq = 0.0
+    orb_eccen = 0.0
+    orb_mvelp = {lon:.4f}
+    orb_mode = 'fixed_parameters'
+    perpetual = .true.
+/
+"""
+
+def main():
+    file_2d = "data/raw/2/NG_5120x2560x34_4km_10s_QOBS_EQX/coarse/2d/all.nc"
+    files_3d = "data/raw/2/NG_5120x2560x34_4km_10s_QOBS_EQX/coarse/3d/*.nc"
+    stat = "data/raw/2/NG_5120x2560x34_4km_10s_QOBS_EQX/stat.nc"
+
+    data = open_and_merge(file_2d, files_3d, stat)
+    iop = prepare_iop_dataset(data)
+
+    # just grab one column
+    # SCAM needs lat and lon to be dimensions
+    # not just coordinates
+    loc = iop.isel(lon=0, lat=32)\
+             .apply(expand_dims)\
+             .transpose('tsec', 'lev', 'lat', 'lon')
+    # for some reason SCAM dies when lon = 0
+    # something to do with initializing the land vegetation array
+    loc.lon[0] = 80
+
+    print("Saving file to disk")
+    loc.to_netcdf("data/processed/iop0x32/iop.nc")
+
+    print("Saving namelist to disk")
+    with open("data/processed/iop0x32/namelist.txt", "w") as f:
+        f.write(namelist_template.format(
+            lat=float(loc.lat),
+            lon=float(loc.lon)))
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
