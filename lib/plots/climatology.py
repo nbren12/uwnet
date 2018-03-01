9803706b92c8fe69c3ab49377f300d3d44617c5a
Noah D. Brenowitz <nbren12@gmail.com>
2018-03-01 15:52:41 -0800

    Start new notebook for plots
    I needed to refactor many of the plotting codes

---
+++ lib/plots/climatology.py
@@ -0,0 +1,70 @@
+import xarray as xr
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def _plot_bias_sig(loc, ax=None, colors=('k', 'b', 'g')):
+
+    dims = ['x', 'time']
+    mu = loc.mean(dims)
+    sig = loc.std(dims)
+    bias = mu - mu.sel(model='Truth')
+
+    ds = xr.Dataset({'mu': mu, 'sig': sig, 'bias': bias})
+
+    lines = {}
+    for c, (model, val) in zip(colors, ds.groupby('model')):
+
+        # show bias only if not plotting the Truth
+        if model == 'Truth':
+            b = val.bias * np.NaN
+        else:
+            b = val.bias
+        b = b.squeeze()
+
+        ax.plot(val.sig.squeeze(), val.p, c=c, ls='--')
+        l, = ax.plot(b, b.p, c=c)
+        # store these later for the legend
+        lines[model] = l
+    ax.set_ylim([1000, 0])
+    return lines
+
+
+def plot_profiles(loc):
+
+    fig, axs = plt.subplots(1, 2, figsize=(4, 3), dpi=100, sharey=True,
+                            gridspec_kw=dict(wspace=0.04))
+
+    lines = _plot_bias_sig(loc.qt, ax=axs[0])
+    lines = _plot_bias_sig(loc.sl, ax=axs[1])
+    axs[1].set_xlim([-2, 4])
+
+    # labels and legends
+    axs[0].set_title(r'$q_T$')
+    axs[0].set_xlabel('(g/kg)')
+    axs[0].set_ylabel('p (hPa)')
+
+    axs[1].set_title(r'$s_L$')
+    axs[1].set_xlabel('K')
+    axs[1].legend(lines.values(), lines.keys(), bbox_to_anchor=(1.0, .9))
+
+    return axs
+
+
+def _plot_pres_vs_lat(bias, ax, levels=np.arange(-5, 6)*.5, title=None):
+    im = ax.contourf(bias.y/1000, bias.p, bias, levels, cmap='bwr',
+                     extend='both')
+
+    plt.colorbar(im, pad=.01, ax=ax)
+    ax.set_xlabel('y (1000 km)')
+    if title:
+        ax.set_title(title)
+
+
+def plot_bias_pres_vs_lat(ds_test, axs):
+    """Plot bias of a dataset on pressure vs latitude"""
+    bias = (ds_test.sel(model='Neural Network').mean(['x', 'time'])
+            - ds_test.sel(model='Truth').mean(['x', 'time']))
+
+    _plot_pres_vs_lat(bias.qt, axs[0], title="Humidity bias (g/kg)")
+    _plot_pres_vs_lat(bias.sl, axs[1], title="Temperature bias (K)")
---
+++ lib/plots/common_data.py
@@ -0,0 +1,72 @@
+import numpy as np
+import pandas as pd
+from pathlib import Path
+
+import lib
+import xarray as xr
+from xnoah import swap_coord
+
+
+def load_data(best_nn="model.VaryNHid-256/2",
+              datadir="../../data"):
+
+    # Truth
+
+    root = Path(datadir)
+    truth = xr.open_dataset(root / "processed/inputs.nc")
+    force = xr.open_dataset(root / "processed/forcings.nc")
+    truth = truth.assign(prec=force.Prec)
+    truth = swap_coord(truth, {'z': 'p'})
+
+    # SCAM
+    # cam = xr.open_dataset("../data/processed/iop/0-8/cam.nc").squeeze()\
+    #         .sel(time=truth.time[:-10])
+
+    # load and interpolate CAM
+    cam = xr.open_dataset(root / "output/scam.nc")
+    cam = lib.pressure_interp_ds(cam, truth.p)
+
+    # neural network scheme
+    best_path =  root / f"output/{best_nn}.columns.nc"
+    nn_cols = xr.open_dataset(best_path).assign(p=truth.p)
+    nn_cols = swap_coord(nn_cols, {'z': 'p'})
+
+    # combine data
+    datasets = [truth, nn_cols, cam]
+    variables = ['qt', 'prec', 'sl']
+    time = np.unique(np.intersect1d(cam.time.values, truth.time.values))
+    data = [ds[variables].sel(time=time) for ds in datasets]
+    model_idx = pd.Index(['Truth', 'Neural Network', 'CAM'], name="model")
+    ds = xr.concat(data, dim=model_idx)
+
+    # change y coordinates
+    y = (ds.y - np.median(ds.y))
+    ds['y'] = y
+
+    return ds
+
+
+def compute_errors(metric, ds, **kwargs):
+
+    mad = {}
+    truth = ds.sel(model='Truth')
+
+    for key in ['Neural Network', 'CAM']:
+        if key in ds.model.values.tolist():
+            mad[key] = metric(
+                truth, ds.sel(model=key), **kwargs).sortby('time')
+
+    mad['Mean'] = metric(truth, truth.mean(['x', 'time']),
+                         **kwargs).sortby('time')
+    mad['Persistence'] = metric(
+        truth, truth.isel(time=0), **kwargs).sortby('time')
+
+    return mad
+
+
+def mean_squared_error(truth, pred, dims=('x', )):
+    return ((truth - pred).fillna(0.0)**2).mean(dims)
+
+
+def mean_absolute_dev(truth, pred, dims=('x', )):
+    return (truth - pred).fillna(0.0).apply(np.abs).mean(dims)
