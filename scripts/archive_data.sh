d91aaa6cbdcc126ddba470999f42ece7ea43ed88
Noah D. Brenowitz <nbren12@gmail.com>
2018-04-02 16:31:35 -0700

    add script for archiving data

---
+++ scripts/archive_data.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+
+find data  -name 'model.torch' -o -name '*.json'\
+          -o -path 'data/output/columns.nc'\
+          -o -path 'data/output/test_error.nc'\
+          -o -path 'data/output/scam.nc'\
+          -o -path 'data/processed/forcings.nc'\
+          -o -path 'data/processed/inputs.nc' | \
+    tar -cvzf data-$(date +%F).tar.gz -T -
