c408696109446ea3238aa4251d1171bed2c5bd50
Noah D. Brenowitz <nbren12@gmail.com>
2018-04-07 00:38:09 -0700

    Remove dead code from lib.util

--- lib/util.py
+++ lib/util.py
@@ -4,24 +4,6 @@ import pandas as pd
 from xnoah.data_matrix import unstack_cat
 
 
-def mat_to_xarray(mat, coord_dict):
-    """Add coordinate information to a numpy array
-
-    Examples
-    --------
-    mat_to_xarray(mat, {1: pd.Index...})
-    """
-
-    coords = []
-    for i in range(mat.ndim):
-        if i in coord_dict:
-            coords.append(coord_dict[i])
-        else:
-            coords.append(pd.Index(range(mat.shape[i]), name="dim_%d" % i))
-
-    return xr.DataArray(mat, coords)
-
-
 def wrap_xarray_calculation(f):
     def fun(*args, **kwargs):
         new_args = []
@@ -34,18 +16,6 @@ def wrap_xarray_calculation(f):
         return f(*new_args, **kwargs)
 
     return fun
-
-
-def xopen(name, nt=20):
-    return xr.open_dataset(name, chunks=dict(time=nt))\
-             .apply(lambda x: x.squeeze())
-
-
-def xopena(name, nt=20):
-    # find variable which isn't p
-    f = xr.open_dataset(name, chunks={'time': nt})
-    varname = [v for v in f.data_vars if v != 'p'][0]
-    return f[varname]
 
 
 def compute_dp(p):
@@ -74,58 +44,3 @@ def mul_if_dims_subset(weight, x):
         return x * weight
     else:
         return x
-
-
-def weights_to_np(w, idx):
-    """
-    TODO Replace with pandas merging functionality
-    """
-
-    def f(i):
-        if i < 0:
-            return 1.0
-        else:
-            return float(w.sel(z=idx.levels[1][i]))
-
-    return np.array([f(i) for i in idx.labels[1]])
-
-
-def scales_to_np(sig, idx):
-    """
-
-    TODO replace with pandas merging functionality
-    """
-
-    def f(i):
-        return float(sig[idx.levels[0][i]])
-
-    return np.array([f(i) for i in idx.labels[0]])
-
-
-def output_to_xr(y, coords):
-    """Create xarray from output
-
-    Parameters
-    ----------
-    y : matrix
-         predictions in matrix format
-    coords:
-         coordinates of the stacked array
-    """
-
-    res = xr.DataArray(y, coords)
-    return unstack_cat(res, "features").unstack("samples")
-
-
-def dict_to_xr(data, dim_name="variable"):
-    """Concatenate a dictionary along a new dimensions name
-    """
-    return xr.concat(data.values(), dim=pd.Index(data.keys(), name=dim_name))
-
-
-def swap_coord(x, **kwargs):
-    for old_dim, (new_coord) in kwargs.items():
-        new_dim = new_coord.name
-        x = x.rename({old_dim: new_dim})\
-             .assign(**{new_dim: new_coord.values})
-    return x
