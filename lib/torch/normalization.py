1a90d1c3761adfca69de2fb3c333dd2431441835
Noah D. Brenowitz <nbren12@gmail.com>
2018-04-07 00:03:50 -0700

    Remove unnecessary function

--- lib/torch/normalization.py
+++ lib/torch/normalization.py
@@ -28,28 +28,3 @@ def scaler(scales, means, x):
         else:
             out[key] = x[key]
     return out
-
-
-def data_to_scalar(data, cuda=False):
-    # compute mean and stddev
-    # this is an error, std does not work like this
-    means = {}
-    scales = {}
-    for key in data['X']:
-        X = data['X'][key]
-        m = X.shape[-1]
-        mu = X.reshape((-1, m)).mean(axis=0)
-        sig = X.reshape((-1, m)).std(axis=0)
-
-        # convert to torch
-        mu, sig = [_numpy_to_variable(np.squeeze(x)) for x in [mu, sig]]
-        sig = torch.mean(sig)
-
-        if cuda:
-            mu = mu.cuda()
-            sig = sig.cuda()
-
-        means[key] = mu
-        scales[key] = sig
-
-    return scaler(scales, means)
