8350d5a94455dd8c463af12c1ec18e7ce3c3cc9a
Noah D. Brenowitz <nbren12@gmail.com>
2018-04-27 16:38:46 -0700

    Add CLI to column_model runner

--- scripts/forced_column_slp.py
+++ scripts/forced_column_slp.py
@@ -2,24 +2,36 @@ import xarray as xr
 import torch
 from lib.torch import column_run, ForcedStepper
 import logging
-
-i = snakemake.input
-RCE = snakemake.params.get('RCE', False)
-
-inputs = xr.open_dataset(i.inputs)
-forcings = xr.open_dataset(i.forcings)
+import hug
 
 
-model = ForcedStepper.load_from_saved(torch.load(i.state))
-model.eval()
+@hug.cli()
+@hug.local()
+def main(inputs: str, forcings: str, state: str, output: str, RCE: bool =False):
 
+    inputs = xr.open_dataset(inputs)
+    forcings = xr.open_dataset(forcings)
 
-if RCE:
-    print("Running in RCE mode (time homogeneous forcings)")
-    inputs = inputs * 0 + inputs.mean('time')
-    forcings = forcings * 0 + forcings.mean('time')
+    model = ForcedStepper.from_file(state)
+    model.eval()
 
-progs, prec = column_run(model, inputs, forcings)
-xr.merge((progs, prec.to_dataset(name="prec")))\
-  .assign(p=inputs.p)\
-  .to_netcdf(snakemake.output[0])
+    if RCE:
+        print("Running in RCE mode (time homogeneous forcings)")
+        inputs = inputs * 0 + inputs.mean('time')
+        forcings = forcings * 0 + forcings.mean('time')
+
+    progs, prec = column_run(model, inputs, forcings)
+    xr.merge((progs, prec.to_dataset(name="prec")))\
+      .assign(p=inputs.p)\
+      .to_netcdf(output)
+
+try:
+    snakemake
+except NameError:
+    main.interface.cli()
+else:
+    main(snakemake.input.inputs,
+     snakemake.input.forcings,
+     snakemake.input.state,
+     snakemake.output[0],
+     **snakemake.params)
