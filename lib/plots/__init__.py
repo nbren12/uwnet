4427c5e684cf31c57a336d0c5e0a3e9dfeeb7958
Noah D. Brenowitz <nbren12@gmail.com>
2018-06-19 00:16:36 -0700

    Cleanup many unused functions and modules
    - Delete many unused functions in lib/thermo
    - Delete lib.util
    - Delete lib.plots

--- lib/plots/__init__.py
+++
@@ -1,4 +0,0 @@
-from .single_column_initial_value import plot_soln
-from . import climatology
-from .common import load_data, hide_xlabels, despine
-from .error_tables import get_error_tables
--- lib/thermo.py
+++ lib/thermo.py
@@ -1,12 +1,29 @@
 import numpy as np
 import xarray as xr
 
-from .util import wrap_xarray_calculation
-
 grav = 9.81
 cp = 1004
 Lc = 2.5104e6
 rho0 = 1.19
+
+
+def metpy_wrapper(fun):
+    """Given a metpy function return an xarray compatible version
+    """
+    from metpy.units import units as u
+
+    def func(*args):
+        def f(*largs):
+            new_args = [u.Quantity(larg, arg.units)
+                        for larg, arg in zip(largs, args)]
+            return fun(*new_args)
+
+        output_units = f(*[1 for arg in args]).units
+        ds = xr.apply_ufunc(f, *args)
+        ds.attrs['units'] = str(output_units)
+        return ds
+
+    return func
 
 
 def omega_from_w(w, rho):
@@ -17,19 +34,6 @@ def omega_from_w(w, rho):
     return - w * rho * grav
 
 
-def q2(d):
-    out = -d.f_qt * Lc/cp / 1000.0
-    out.attrs['units'] = 'K/d'
-    return out.to_dataset(name='Q2')
-
-
-def q1(d):
-    out = d.f_sl
-    out.attrs['units'] = 'K/d'
-    return out.to_dataset(name='Q1')
-
-
-@wrap_xarray_calculation
 def liquid_water_temperature(t, qn, qp):
     """This is an approximate calculation neglecting ice and snow
     """
@@ -39,14 +43,6 @@ def liquid_water_temperature(t, qn, qp):
     return sl
 
 
-def d_liquid_water_temperature(divt, divqn, divqp):
-    sl = divt - Lc/cp * (divqp + divqn)/1000.0
-    sl.attrs['units'] = 'K/s'
-
-    return sl
-
-
-@wrap_xarray_calculation
 def total_water(qv, qn):
     qt = qv + qn
     qt.attrs['units'] = 'g/kg'
@@ -89,50 +85,9 @@ def mass_integrate(p, x, average=False):
     return ans
 
 
-def metpy_wrapper(fun):
-    """Given a metpy function return an xarray compatible version
-    """
-    from metpy.units import units as u
-
-    def func(*args):
-        def f(*largs):
-            new_args = [u.Quantity(larg, arg.units)
-                        for larg, arg in zip(largs, args)]
-            return fun(*new_args)
-
-        output_units = f(*[1 for arg in args]).units
-        ds = xr.apply_ufunc(f, *args)
-        ds.attrs['units'] = str(output_units)
-        return ds
-
-    return func
-
 
 def column_rh(QV, TABS, p):
     from metpy.calc import relative_humidity_from_mixing_ratio
     rh = metpy_wrapper(relative_humidity_from_mixing_ratio)(QV, TABS, p)
 
     return mass_integrate(p, rh/1000, average=True)
-
-
-def precip_from_ds(dsl, qrad, shf, p):
-    return (cp*mass_integrate(p, dsl) - cp*mass_integrate(p, qrad)
-            - shf*86400)/Lc
-
-
-def precip_from_dq(dq, lhf, p):
-    return - mass_integrate(p, dq/1000) + lhf/Lc*86400
-
-
-def mse(sl, qt):
-    return cp * sl + qt/1000 * Lc
-
-
-def shf_to_tendency(shf, rho0):
-    rhodz = float(rho0.z * 2 * rho0)
-    return shf/cp*86400/rhodz
-
-
-def lhf_to_tendency(lhf, rho0):
-    rhodz = float(rho0.z * 2 * rho0)
-    return lhf/Lc*86400/rhodz
