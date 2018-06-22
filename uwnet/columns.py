4ec0f7adf593ccf1255d1e67cd9f4e51988e3552
Noah D. Brenowitz <nbren12@gmail.com>
2018-06-22 16:54:17 -0700

    Allow 2D outputs in single column model script

--- uwnet/columns.py
+++ uwnet/columns.py
@@ -15,14 +15,16 @@ from uwnet.data import get_dataset
 def parse_arguments():
     parser = argparse.ArgumentParser(description='')
     parser.add_argument('model')
+    parser.add_argument('config')
+    parser.add_argument('output')
     return parser.parse_args()
 
 
 # load configuration and process arguments
-config = yaml.load(open("config.yaml"))
 args = parse_arguments()
 
 # load model
+config = yaml.load(open(args.config))
 d = torch.load(args.model)
 mod = model.MLP.from_dict(d['dict'])
 
@@ -35,8 +37,10 @@ def post(x):
 
 
 paths = config['paths']
+print("Opening data")
 data = get_dataset(paths, post=post)
 
+print("Running model")
 # prepare input for mod
 with torch.no_grad():
     batch = {key: torch.from_numpy(val) for key, val in data[:].items()}
@@ -51,13 +55,17 @@ def unstack(val):
     dims = ['xbatch', 'xtime', 'xfeat'][:val.ndim]
     coords = {key: data._ds.coords[key] for key in dims}
 
+    if val.shape[-1] == 1:
+        dims.pop()
+        coords.pop('xfeat')
+        val = val[..., 0]
     ds = xr.DataArray(val, dims=dims, coords=coords)
     for dim in dims:
         ds = ds.unstack(dim)
 
     return ds
 
-
+print("Reshaping and saving outputs")
 out_da = {key: unstack(val) for key, val in out.items()}
-ds = xr.Dataset(out_da).merge(data.data.rename({'qt': 'QTOBS', 'sl': 'SLOBS'}))
-ds.to_netcdf("out.nc")
+ds = xr.Dataset(out_da).merge(data.data.rename({key: key + 'OBS' for key in out}))
+ds.to_netcdf(args.output)
