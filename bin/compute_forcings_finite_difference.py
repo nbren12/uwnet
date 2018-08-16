d5a8ca07c9c6a8c673330de0335de5a70204aaf3
Noah D. Brenowitz <nbren12@gmail.com>
2018-08-15 21:58:01 -0700

    Validate input data in forcing calculation
    The domain mean of the vertical velocity should approximately vanish.
    This is now tested before the advection forcings are estimated.

--- bin/compute_forcings_finite_difference.py
+++ bin/compute_forcings_finite_difference.py
@@ -6,6 +6,23 @@ import xarray as xr
 from gnl.calculus import c_grid_advective_tendency
 
 
+def validate_inputs(ds):
+    # vertical velocity should vanish
+    mean = ds.W[0].mean(['x', 'y'])
+    sig = ds.W[0].std(['x', 'y'])
+
+    M = ds.layer_mass.sum()
+
+    mean_ss = np.sqrt(float((ds.layer_mass * (mean**2)).sum()/M))
+    sig_ss = np.sqrt(float((ds.layer_mass * (sig**2)).sum()/M))
+
+    if (mean_ss / sig_ss) > 1e-4:
+        raise ValueError(
+            "The domain mean of W is larger than acceptable. The typical "
+            f"magnitude is {mean_ss} while the standard deviation is {sig_ss}."
+        )
+
+
 def compute_advection_tedency_xarray(g, key):
 
     # handle grid information
@@ -33,6 +50,7 @@ if __name__ == '__main__':
     data, out = sys.argv[1:]
 
     g = xr.open_zarr(data)
+    validate_inputs(g)
     forcings = xr.Dataset(
         dict(
             FQT=compute_advection_tedency_xarray(g, 'qt'),
