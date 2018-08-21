10ce507c3c632aefa11224d2b4071c4d42887028
Noah D. Brenowitz <nbren12@gmail.com>
2018-08-21 01:54:39 -0700

    Add functions for reading debugging data
    these functions can both load the torch.saved outputs, convert them to
    xarray, and merge these datasets with the training data. This allows for
    quick visualiation in the ipython notebooks

---
+++ uwnet/analysis/sam_debug.py
@@ -0,0 +1,93 @@
+"""Routines for opening and analyzing debugging output from SAM/UWNET"""
+import glob
+import os
+from collections.abc import Mapping
+
+import pandas as pd
+
+import torch
+import xarray as xr
+
+
+def index_like(x, y):
+    if isinstance(x, Mapping):
+
+        keys = set(x) & set(y.data_vars)
+        return xr.Dataset({key: index_like(x[key], y[key]) for key in keys})
+    else:
+        if x.shape[0] == 1:
+            x = x[0]
+        return xr.DataArray(x, dims=y.dims, coords=y.coords)
+
+
+def open_debug_state_like_ds(path: str, ds: xr.Dataset) -> xr.Dataset:
+    """Open SAM debugging output as xarray object
+
+    Parameters
+    ----------
+    path
+        path to pickle saved by torch
+    ds
+        dataset to use a template
+
+    Returns
+    -------
+    state
+        dataset with fields from path
+    """
+    dbg = torch.load(path)
+    state = index_like(dbg['args'][0], ds)
+    out = index_like(dbg['out'], ds)
+    out = out.rename({key: key + 'NN' for key in set(state.data_vars) & set(out.data_vars)})
+    return xr.merge([state, out])
+
+
+def concat_datasets(args, name='mode'):
+    """Concatenate datasets with a new named index
+
+    This function is especially useful for comparing two datasets with
+    shared variables with holoviews
+
+    Parameters
+    ----------
+    args : list
+        list of (name, dataset) pairs
+    name : str
+        name of the new dimension
+
+    Returns
+    -------
+    ds : xr.Dataset
+        concatenated data
+    """
+
+    names, vals = zip(*args)
+
+    # get list of vars
+    vars = set(vals[0].data_vars)
+    for val in vals:
+        vars = vars & set(val.data_vars)
+    vars = list(vars)
+
+    vals = [val[vars] for val in vals]
+
+    return xr.concat(vals, dim=pd.Index(names, name=name))
+
+
+def open_debug_and_training_data(t, ids, training_data_path):
+    """Open an concatenate the debugging and training data"""
+
+    debug_files = {
+        tag: glob.glob(os.path.join(path, '*.pkl'))
+        for tag, path in ids.items()
+    }
+
+    # open training
+    training_ds = xr.open_dataset(training_data_path)
+    train_ds_init_time = training_ds.isel(time=0)
+
+    args = [('Train', train_ds_init_time)]
+    for tag in debug_files:
+        dbg = open_debug_state_like_ds(debug_files[tag][t], train_ds_init_time)
+        args.append((tag, dbg))
+    return concat_datasets(args, name='tag')
