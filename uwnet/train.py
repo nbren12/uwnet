6b0683d6471810bc2c475e0794b49ddcdef3842e
Noah D. Brenowitz <nbren12@gmail.com>
2018-09-18 16:30:44 -0700

    Do not train on upper atmosphere
    I added a vertical_grid_size option to the configuration file, which
    determines how many vertical grid points to use when training.

--- uwnet/train.py
+++ uwnet/train.py
@@ -77,7 +77,9 @@ def main():
         ds = xr.open_zarr(args.input)
     except ValueError:
         ds = xr.open_dataset(args.input)
+
     nt = len(ds.time)
+    ds = ds.isel(z=slice(0, config['vertical_grid_size']))
     train_data = XRTimeSeries(ds.load(), [['time'], ['x', 'y'], ['z']])
     train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True)
     constants = train_data.torch_constants()
