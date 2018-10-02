0d7e42f8dfc21ccc9b2c7d0f3e5d9236e5797348
Noah D. Brenowitz <nbren12@gmail.com>
2018-10-02 11:55:05 -0700

    Unify the SAM running command line interfaces
    Previously I had two scripts. One for running initial condition (IC)
    runs with SAM, and the other for running IC runs with the neural
    network. I have merged these into one script. Also, this script is best
    for setting up a case directory.
    
    Resolves #20

--- src/criticism/run_sam_ic_nn.py
+++ src/criticism/run_sam_ic_nn.py
@@ -9,21 +9,29 @@ NGAQUA_ROOT = "/Users/noah/Data/2018-05-30-NG_5120x2560x34_4km_10s_QOBS_EQX"
 
 
 @click.command()
-@click.argument('model', type=click.Path())
+@click.argument('path')
+@click.option(
+    '-nn',
+    '--neural-network',
+    type=click.Path(),
+    help='use the neural network in this pickled model file.')
 @click.option('-n', '--ngaqua-root', type=click.Path(), default=NGAQUA_ROOT)
 @click.option('-t', type=int, default=0)
-def main(model, ngaqua_root, t):
+@click.option('-r', '--run', is_flag=True)
+@click.option('-d', '--docker-image', type=str, default='nbren12/uwnet')
+def main(path, neural_network, ngaqua_root, t, run, docker_image):
+    """Create SAM case directory for an NGAqua initial value problem and optionally
+    run the model with docker.
+
+    """
 
     ic = get_ngqaua_ic(ngaqua_root, t)
 
     case = InitialConditionCase(
-        path='test',
-        ic=ic,
-        sam_src="ext/sam/",
-        docker_image='nbren12/uwnet')
+        path=path, ic=ic, sam_src="ext/sam/", docker_image=docker_image)
 
     case.prm['parameters']['dodamping'] = True
-    case.prm['parameters']['khyp'] = 5e16
+    case.prm['parameters']['khyp'] = 1e16
 
     dt = 120.0
     day = 86400
@@ -46,28 +54,30 @@ def main(model, ngaqua_root, t):
 
     case.prm['parameters']['dosgs'] = True
     case.prm['parameters']['dosurface'] = True
-    case.prm['python']['dopython'] = False
 
-    # setup the neural network
-    case.prm['python'].update(
-        dict(
-            dopython=True,
-            usepython=True,
-            npython=1,
-            function_name='call_neural_network',
-            module_name='uwnet.sam_interface'))
+    # configure neural network run
+    if neural_network:
+        case.prm['python']['dopython'] = False
 
-    # configure needed environmental variables
-    case.mkdir()
-    model_run_path = 'model.pkl'
-    case.add(model, model_run_path)
-    case.env.update(dict(
-        UWNET_MODEL=model_run_path
-    ))
+        # setup the neural network
+        case.prm['python'].update(
+            dict(
+                dopython=True,
+                usepython=True,
+                npython=1,
+                function_name='call_neural_network',
+                module_name='uwnet.sam_interface'))
+
+        print(f"Copying {neural_network} to model directory")
+        case.mkdir()
+        model_run_path = 'model.pkl'
+        case.add(neural_network, model_run_path)
+        case.env.update(dict(UWNET_MODEL=model_run_path))
 
     case.save()
 
-    case.run_docker()
+    if run:
+        case.run_docker()
 
 
 if __name__ == '__main__':
