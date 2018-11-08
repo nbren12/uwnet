99a9535a70786b525fb0c5abb7d0b1fc3f82b2c0
Noah D. Brenowitz <nbren12@gmail.com>
2018-11-08 13:41:55 -0800

    Add a TensorDict class
    This class simplifies operations on dicts of tensors by mapping binary and unary
    operations accross the values of the contained dictionary.

---
+++ uwnet/tensordict.py
@@ -0,0 +1,54 @@
+from collections import Mapping, KeysView
+from abc import ABCMeta
+from toolz import curry
+import attr
+
+mathfuns = [
+    '__add__', '__rmul__', '__mul__', '__sub__', '__radd__', '__rmul__',
+    '__truediv__', '__floordiv__', '__pow__', '__rpow__'
+]
+
+dictfuns = ['__len__', '__iter__']
+
+
+@curry
+def valmap_binary_operator(name, self, other):
+    out = {}
+    for key in self.data:
+        func = getattr(self.data[key], name)
+        if isinstance(other, self.__class__):
+            out[key] = func(other.data[key])
+            if self.keys() != other.keys():
+                raise ValueError("Both arguments to binary arguments must "
+                                 "have the same keys.")
+        else:
+            out[key] = func(other)
+    return TensorDict(out)
+
+
+@curry
+def valmap_unary_operator(name, attrname, self):
+    func = getattr(getattr(self, attrname), name)
+    return func()
+
+
+class ArithmaticMeta(ABCMeta):
+    def __new__(cls, name, bases, dct):
+        for fun in mathfuns:
+            dct[fun] = valmap_binary_operator(fun)
+        for fun in dictfuns:
+            dct[fun] = valmap_unary_operator(fun, 'data')
+        return super().__new__(cls, name, bases, dct)
+
+
+@attr.s
+class TensorDict(Mapping, metaclass=ArithmaticMeta):
+    """Wrapper which overloads operators for dicts of tensors"""
+    data = attr.ib()
+
+    def __getitem__(self, key):
+        if isinstance(key, (list, KeysView, self.__class__)):
+            keys = list(key)
+            return TensorDict({key: self.data[key] for key in keys})
+        else:
+            return self.data[key]
