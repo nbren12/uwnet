975317f7c06345e0b4f1824bc9f4626ce407d694
Noah D. Brenowitz <nbren12@gmail.com>
2018-11-19 14:41:25 -0800

    Add linearized analysis functions
    I hypothesize that blow-up in SAM is connected to linearly unstable
    modes of the neural network. This commit introduces code for computing
    the jacobian and it's eigenvalues in an autodiff friendly way. Also,
    the weak temperature gradient in the tropics significantly alters what
    the unstable modes will look like, so I include code that penalizes
    unstable WTG modes.
    
    These changs required some additions to the Batch class in the
    timestepper module.

---
+++ uwnet/test_wtg.py
@@ -0,0 +1,28 @@
+import torch
+from .wtg import wtg_penalty
+from .loss import weighted_mean_squared_error
+from .timestepper import Batch
+from .tensordict import TensorDict
+
+
+def test_wtg():
+    """This integration test ensures that the code runs
+
+    TODO: this test depends on too much, I need to make it better
+    """
+
+    z = torch.arange(0, 10e3, 1e3)
+    H = 5e3
+    weights = torch.exp(-z / H)
+    qt = torch.exp(-z / H) * 16.0
+    sli = z * .004 + 290
+    criterion = weighted_mean_squared_error(weights=weights, dim=-3)
+
+    d = {'QT': qt.view(1, -1, 1, 1), 'SLI': sli.view(1, -1, 1, 1)}
+    prognostics = ['QT', 'SLI']
+    batch = Batch(d, prognostics)
+
+    def model(x):
+        return TensorDict({'QT': - x['QT']/5.0, 'SLI': -x['QT']*2})
+
+    wtg_penalty(model, z, batch)
