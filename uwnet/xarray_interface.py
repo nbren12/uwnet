041201b1c53c350c299602966f456db4efd55869
Noah D. Brenowitz <nbren12@gmail.com>
2018-12-14 19:15:07 -0500

    Add tests and error messages for xarray interface

--- uwnet/xarray_interface.py
+++ uwnet/xarray_interface.py
@@ -13,7 +13,9 @@ def _array_to_tensor(da):
     if _is_at_least_2d(da):
         if 'z' not in da.dims:
             da = da.expand_dims('z', -3)
-    return torch.from_numpy(da.values).detach().float()
+    return torch.from_numpy(da.values)\
+                .detach()\
+                .float()
 
 
 def _dataset_to_torch_dict(ds):
@@ -33,13 +35,11 @@ def _torch_dict_to_dataset(output, coords):
     time_dim_present = 'time' in dims_2d
 
     for key, val in output.items():
-        if time_dim_present:
-            nz = val.size(1)
-        else:
-            nz = val.size(0)
+        z_dim = 1 if time_dim_present else 0
+        nz = val.size(z_dim)
 
         if nz == 1:
-            data_vars[key] = (dims_2d, output[key].numpy().squeeze())
+            data_vars[key] = (dims_2d, output[key].numpy().squeeze(z_dim))
         else:
             data_vars[key] = (dims_3d, output[key].numpy())
 
@@ -48,12 +48,23 @@ def _torch_dict_to_dataset(output, coords):
     return xr.Dataset(data_vars, coords=coords)
 
 
-def call_with_xr(self, ds, drop_times=1, **kwargs):
+def _assert_no_null_dimensions(ds):
+    for key in ds.data_vars:
+        for k, n in enumerate(ds[key].shape):
+            if ds[key].shape[k] == 0:
+                dim = ds[key].dims[k]
+                raise ValueError(
+                    f"'{dim}' dimension of '{key}' has length 0"
+                )
+
+
+def call_with_xr(self, ds, **kwargs):
     """Call the neural network with xarray inputs"""
+    _assert_no_null_dimensions(ds)
     tensordict = _dataset_to_torch_dict(ds)
     with torch.no_grad():
         output = self(tensordict, **kwargs)
-    return _torch_dict_to_dataset(output, ds.coords, drop_times)
+    return _torch_dict_to_dataset(output, ds.coords)
 
 
 def _numpy_dict_to_torch_dict(inputs):
@@ -73,10 +84,4 @@ def call_with_numpy_dict(self, inputs, **kwargs):
 
 class XRCallMixin(object):
     """PyTorch module for predicting Q1, Q2 and maybe Q3"""
-
-    def call_with_xr(self, ds, **kwargs):
-        """Call the neural network with xarray inputs"""
-        tensordict = _dataset_to_torch_dict(ds)
-        with torch.no_grad():
-            output = self(tensordict, **kwargs)
-        return _torch_dict_to_dataset(output, ds.coords)
+    call_with_xr = call_with_xr
