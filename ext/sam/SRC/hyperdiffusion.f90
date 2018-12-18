23a252be0e8b3d9fdcf4f99c092880612462a63f
Noah D. Brenowitz <nbren12@gmail.com>
2018-12-18 09:23:41 -0800

    Add hyperdiffusion of humidity
    The humidity field showed obvious 2 dx oscillations. These should be
    smoothed by damping.

--- ext/sam/SRC/hyperdiffusion.f90
+++ ext/sam/SRC/hyperdiffusion.f90
@@ -2,6 +2,7 @@ module hyperdiffusion
 contains
   subroutine hyper_diffuse
     use vars
+    use microphysics, only: micro_field, index_water_vapor
     use params, only: docolumn, dowallx, dowally, khyp
     implicit none
 
@@ -87,7 +88,7 @@ contains
     end if
 
 
-    !  Add hyperdiffusive terms to the momentum
+    !  Add hyperdiffusive terms to the momentum, temperature and scalars
 
     do k=1,nzm
        do j = 1,ny
@@ -101,15 +102,14 @@ contains
                   (v(i-2, j, k) - 4*v(i-1,j,k) + 6*v(i,j,k) - 4*v(i+1,j,k) + v(i+2,j,k)) + &
                   rdy16* &
                   (v(i, j-2, k) - 4*v(i,j-1,k) + 6*v(i,j,k) - 4*v(i,j+1,k) + v(i,j+2,k)) )
-
-             tsrc = - khyp * (rdx16 *  &
-                  (t(i-2, j, k) - 4*t(i-1,j,k) + 6*t(i,j,k) - 4*t(i+1,j,k) + t(i+2,j,k)) + &
-                  rdy16* &
-                  (t(i, j-2, k) - 4*t(i,j-1,k) + 6*t(i,j,k) - 4*t(i,j+1,k) + t(i,j+2,k)) )
-             t(i,j,k) = t(i,j,k) + tsrc * dtn
           end do
        end do
     end do
+
+    ! scalar
+    call hyper_diffuse_scalar(micro_field(:,:,:,index_water_vapor))
+    call hyper_diffuse_scalar(t)
+
 
     do k=2,nzm
        do j = 1,ny
@@ -125,4 +125,76 @@ contains
 
 
   end subroutine hyper_diffuse
+
+  subroutine hyper_diffuse_scalar(t)
+    use grid
+    use params, only: docolumn, dowallx, dowally, khyp
+    real, intent(inout) :: t(dimx1_s:dimx2_s, dimy1_s:dimy2_s, nzm)
+
+    ! local variables
+    real tsrc, rdx16, rdy16, rdx2, rdy2, rdx25, rdy25
+
+    rdx2=1./(dx*dx)
+    rdy2=1./(dy*dy)
+
+    rdx25=0.25*rdx2
+    rdy25=0.25*rdy2
+
+    rdx16 = rdx25*rdx25
+    rdy16 = rdy25*rdy25
+
+    if(dowallx) then
+
+       if(mod(rank,nsubdomains_x).eq.0) then
+          do k=1,nzm
+             do j=1,ny
+                t(0,j,k) = t(1,j,k)
+             end do
+          end do
+       end if
+       if(mod(rank,nsubdomains_x).eq.nsubdomains_x-1) then
+          do k=1,nzm
+             do j=1,ny
+                t(nx+1,j,k) = t(nx,j,k)
+             end do
+          end do
+       end if
+
+    end if
+
+    if(dowally) then
+
+       if(rank.lt.nsubdomains_x) then
+          do k=1,nzm
+             do i=1,nx
+                t(i,1-YES3D,k) = t(i,1,k)
+             end do
+          end do
+       end if
+       if(rank.gt.nsubdomains-nsubdomains_x-1) then
+          do k=1,nzm
+             do i=1,nx
+                t(i,ny+YES3D,k) = t(i,ny,k)
+             end do
+          end do
+       end if
+
+    end if
+
+
+    !  Add hyperdiffusive terms to the momentum, temperature and scalars
+    do k=1,nzm
+       do j = 1,ny
+          do i=1,nx
+             tsrc = - khyp * (rdx16 *  &
+                  (t(i-2, j, k) - 4*t(i-1,j,k) + 6*t(i,j,k) - 4*t(i+1,j,k) + t(i+2,j,k)) + &
+                  rdy16* &
+                  (t(i, j-2, k) - 4*t(i,j-1,k) + 6*t(i,j,k) - 4*t(i,j+1,k) + t(i,j+2,k)) )
+             t(i,j,k) = t(i,j,k) + tsrc * dtn
+          end do
+       end do
+    end do
+
+  end subroutine hyper_diffuse_scalar
+
 end module hyperdiffusion
