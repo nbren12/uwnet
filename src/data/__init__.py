af33be8a459a5c0275a45a1f732a578fa3f02c83
Noah D. Brenowitz <nbren12@gmail.com>
2019-01-14 18:34:41 -0800

    Analyze the bias in the nudging run
    - not enough Q1 in the tropics => circulation bias

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -1,6 +1,6 @@
 from .sam import SAMRun
+import xarray as xr
 from pathlib import Path
-
 
 _this_file = Path(__file__)
 
@@ -19,3 +19,18 @@ training_data = str(root / "data/processed/training.nc")
 ngaqua_climate_path = str(root / "data/processed/training.mean.nc")
 
 ngaqua = root / "data/raw/2018-05-30-NG_5120x2560x34_4km_10s_QOBS_EQX/"
+
+
+def open_data(tag):
+    """Open commonly used datasets"""
+    if tag == "training":
+        return xr.open_dataset(training_data).isel(step=0).drop('step')
+    if tag == 'training_with_src':
+        from uwnet.thermo import compute_apparent_source
+        ds = open_data('training')
+        return ds.assign(
+            Q1=compute_apparent_source(ds.SLI, 86400*ds.FSLI),
+            Q2=compute_apparent_source(ds.QT, 86400*ds.FQT),
+        )
+    else:
+        return NotImplementedError
