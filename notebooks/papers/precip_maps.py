2cd079984250e34a298775a9ce5a54cf225c9a56
Noah D. Brenowitz <nbren12@gmail.com>
2019-01-22 17:17:38 -0800

    improve colormap in precip plots

--- notebooks/papers/precip_maps.py
+++ notebooks/papers/precip_maps.py
@@ -2,6 +2,23 @@ import xarray as xr
 
 from src.data import open_data, runs
 from uwnet.thermo import lhf_to_evap
+from matplotlib import colors
+import numpy as np
+
+
+
+# Example of making your own norm.  Also see matplotlib.colors.
+# From Joe Kington: This one gives two different linear ramps:
+class MidpointNormalize(colors.Normalize):
+    def __init__(self, vmin=None, vmax=None, midpoint=None, clip=False):
+        self.midpoint = midpoint
+        colors.Normalize.__init__(self, vmin, vmax, clip)
+
+    def __call__(self, value, clip=None):
+        # I'm ignoring masked values and all kinds of edge cases to make a
+        # simple example...
+        x, y = [self.vmin, self.midpoint, self.vmax], [0, 0.5, 1]
+        return np.ma.masked_array(np.interp(value, x, y))
 
 
 def get_data():
@@ -19,7 +36,15 @@ def get_data():
 
 
 def plot(data):
-    data.to_array(dim='Run').plot(col='Run', vmax=100, vmin=-10)
+    norm = MidpointNormalize(midpoint=0, vmin=-20, vmax=50)
+    data = data.assign_coords(x=data.x/1e6, y=data.y/1e6)
+    grid = data.to_array(dim='Run').plot(col='Run', norm=norm, add_labels=False)
+
+    grid.set_xlabels("x (1000 km)")
+    grid.set_ylabels("y (1000 km)")
+
+    grid.axes[0,0].set_title("a) NG-Aqua", loc='left')
+    grid.axes[0,1].set_title("b) NN", loc='left')
 
 
-plot(get_data())
+grid = plot(get_data())
