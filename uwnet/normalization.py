7b4a7584a68b4b7be35eb88c0e0c634b702ca4f3
Noah D. Brenowitz <nbren12@gmail.com>
2019-01-31 00:28:31 -0800

    Test the new Scaler class methods

--- uwnet/normalization.py
+++ uwnet/normalization.py
@@ -2,15 +2,14 @@ import logging
 from functools import reduce
 
 from toolz.curried import valmap
+from toolz import first
 
 import torch
 from torch import nn
 
+import xarray as xr
+
 logger = logging.getLogger(__name__)
-
-
-def _numpy_to_variable(x):
-    return torch.tensor(x).float()
 
 
 def _scale_var(scale, mean, x):
@@ -24,16 +23,6 @@ def _scale_var(scale, mean, x):
     return x.float()
 
 
-def scaler(scales, means, x):
-    out = {}
-    for key in x:
-        if key in scales and key in means:
-            out[key] = _scale_var(scales[key], means[key], x[key])
-        else:
-            out[key] = x[key]
-    return out
-
-
 def _dict_to_parameter_dict(x):
     out = {}
     for key in x:
@@ -41,37 +30,40 @@ def _dict_to_parameter_dict(x):
     return nn.ParameterDict(out)
 
 
-def _get_scaler_args_numpy(dataset):
+def _convert_dataset_to_torch_dict(ds: xr.Dataset):
+    return {key: torch.from_numpy(ds[key].values)
+            for key in ds.data_vars}
+
+
+def _compute_scaler_data_from_xarray(dataset):
     logger.info("Computing mean")
     mean = dataset.mean(['x', 'y', 'time'])
     logger.info("Computing std")
     scale = dataset.std(['x', 'y', 'time'])
-    return mean, scale
+
+    args = [valmap(torch.squeeze, _convert_dataset_to_torch_dict(arg))
+            for arg in [mean, scale]]
+    return args
 
 
-def get_mean_scale(dataset):
-    from .datasets import _ds_slice_to_torch
-    out = map(_ds_slice_to_torch, _get_scaler_args_numpy(dataset))
-    return map(valmap(torch.squeeze), out)
-
-
-def add_tuple(x, y):
+def add_tuples(x, y):
     return tuple(xx + yy for xx, yy in zip(x, y))
 
 
 def moments(batch):
     data = batch.data.double()
-    shape = data['QT'].shape
+    first_variable = first(data.values())
+    shape = first_variable.shape
     num_examples = shape[1] * shape[0]
     m1 = data.sum(0).sum(0)
-    m2 = (data**2).sum(0).sum(0)
+    m2 = (data ** 2).sum(0).sum(0)
     return num_examples, m1, m2
 
 
 def moments_from_data_loader(loader):
-    n, m1, m2 = reduce(add_tuple, map(moments, iter(loader)))
+    n, m1, m2 = reduce(add_tuples, map(moments, iter(loader)))
     m1 = m1 / n
-    v = m2 / n - m1**2
+    v = m2 / n - m1 ** 2
     return m1, v.sqrt()
 
 
@@ -81,6 +73,8 @@ class Scaler(nn.Module):
     def __init__(self, mean=None, scale=None):
         "docstring"
         super(Scaler, self).__init__()
+        self.mean = mean
+        self.scale = scale
 
     def forward(self, x):
         out = {}
@@ -96,7 +90,7 @@ class Scaler(nn.Module):
         self.scale = _dict_to_parameter_dict(scale)
 
     def fit_xarray(self, dataset):
-        mean, scale = get_mean_scale(dataset)
+        mean, scale = _compute_scaler_data_from_xarray(dataset)
         self.set_mean_scale(mean, scale)
         return self
 
