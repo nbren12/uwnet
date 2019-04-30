10b38b83706ed8b336eac050a518f5ea73960ce6
Noah D. Brenowitz <nbren12@gmail.com>
2019-04-30 15:57:44 -0700

    Compile different resolutions in docker image

--- setup/docker/compile_sam.sh
+++ setup/docker/compile_sam.sh
@@ -3,4 +3,9 @@
 # must be run from root directory of uwnet
 
 UWNET=$(pwd)
-docker-compose run -w /opt/sam sam  ./Build
+
+docker-compose run -w /opt/sam -e NX=512 -e NY=256 -e NZ=34 \
+    -e NSUBX=2 -e NSUBY=2 sam ./Build
+
+docker-compose run -w /opt/sam -e NX=128 -e NY=64 -e NZ=34 \
+    -e NSUBX=1 -e NSUBY=1 sam ./Build
