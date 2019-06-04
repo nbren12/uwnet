Updated examine_stochastic_state_model to use the correct sign for net precip

uwnet/stochastic_parameterization/examine_stochastic_state_model.py
@@ -344,28 +344,28 @@ def compare_true_to_simulated_q1_q2_distributions(
     ax[0].set_xlabel('W/m2')
     ax[0].set_ylabel('Log Density')
     if true_etas:
-        title = 'Log PDF for NN Net Heating, with True Eta Transitions'
+        title = 'Log PDF for NN Net Heating, with True Markov States'
     else:
         title = 'Log PDF for NN Net Heating'  # noqa
     ax[0].title.set_text(title_prefix + title)
     loghist(
         qts_true,
         ax=ax[1],
-        lower_percentile=.01,
+        upper_percentile=99.99,
         label='True Data',
         gaussian_comparison=False
     )
     loghist(
         qts_pred,
         ax=ax[1],
-        lower_percentile=.01,
+        upper_percentile=99.99,
         label='Stochastic Paramterization',
         gaussian_comparison=False
     )
     loghist(
         qts_pred_base,
         ax=ax[1],
-        lower_percentile=.01,
+        upper_percentile=99.99,
         label='NN Parameterization',
         gaussian_comparison=False
     )
@@ -374,7 +374,7 @@ def compare_true_to_simulated_q1_q2_distributions(
     ax[1].set_ylabel('Log Density')
     ax[1]
     if true_etas:
-        title = 'Log PDF for NN Net Precip, with True Eta Transitions'
+        title = 'Log PDF for NN Net Precip, with True Markov States'
     else:
         title = 'Log PDF for NN Net Precip'  # noqa
     ax[1].title.set_text(title_prefix + title)
@@ -404,6 +404,6 @@ if __name__ == '__main__':
     # model.train()
     model = torch.load(dir_ + 'stochastic_model_mlp.pkl')
     # evaluate_stochasticity_of_model(model)
-    plot_true_eta_vs_simulated_eta(model)
+    # plot_true_eta_vs_simulated_eta(model)
     compare_true_to_simulated_q1_q2_distributions(
         model, base_model, true_etas=False, only_tropics=False)

uwnet/stochastic_parameterization/presentation_figures.py
@@ -16,14 +16,14 @@ style = 'viridis'
 
 dir_ = '/Users/stewart/projects/uwnet/uwnet/stochastic_parameterization/'
 ds_location = dir_ + 'training.nc'
-ds = xr.open_dataset(ds_location).isel(time=range(10))
+ds = xr.open_dataset(ds_location).isel(time=range(80))
 
 
-def plot_net_precip(model, ds):
-    fig, ax = plt.subplots(1, 2, sharex=True, sharey=True)
+def plot_net_precip(model_s, model_b, ds):
+    fig, ax = plt.subplots(1, 3, sharex=True, sharey=True)
     true = -(compute_apparent_source(
-        ds.QT, ds.FQT * 86400).isel(time=0).dot(ds.layer_mass)
-        / liquid_water_density
+        ds.QT, ds.FQT * 86400).isel(time=0).dot(ds.layer_mass) /
+        liquid_water_density
      )
     true.plot(
         ax=ax[0],
@@ -32,17 +32,29 @@ def plot_net_precip(model, ds):
         cmap=style,
         vmax=100)
     ax[0].title.set_text('NG-Aqua Net Precip')
-    pred = -(model.predict(ds.isel(time=[0]))['QT'].dot(
+    pred_b = -(model_b.predict(ds.isel(time=[0]))['QT'].dot(
         ds.layer_mass).isel(time=0) / liquid_water_density)
-    pred.attrs['units'] = 'mm/day'
-    pred.attrs['long_name'] = 'Net Precip.'
-    pred = pred.rename('Net Precip.')
-    pred.plot(
+    pred_b.attrs['units'] = 'mm/day'
+    pred_b.attrs['long_name'] = 'Net Precip.'
+    pred_b = pred_b.rename('Net Precip.')
+    pred_b.plot(
         ax=ax[1],
+        add_colorbar=False,
         vmin=-100,
         vmax=100,
         cmap=style)
-    ax[1].title.set_text('NN-Predicted Net Precip')
+
+    pred_s = -(model_s.predict(ds.isel(time=[0]))['QT'].dot(
+        ds.layer_mass).isel(time=0) / liquid_water_density)
+    pred_s.attrs['units'] = 'mm/day'
+    pred_s.attrs['long_name'] = 'Net Precip.'
+    pred_s = pred_s.rename('Net Precip.')
+    pred_s.plot(
+        ax=ax[2],
+        vmin=-100,
+        vmax=100,
+        cmap=style)
+    ax[2].title.set_text('Stochastic Model Net Precip')
     fig.suptitle('Net Precip Comparison')
     plt.show()
 
@@ -75,7 +87,57 @@ def plot_net_heating(model, ds):
     plt.show()
 
 
+def plot_2d(var):
+    ds[var].isel(time=0).plot()
+    long_name = ds[var].attrs['long_name']
+    plt.title(f'NG-Aqua {long_name} at Time = 0')
+    plt.show()
+
+
+def plot_2d_over_time():
+    for var in ['SST', 'SOLIN']:
+        long_name = ds[var].attrs['long_name']
+        ds[var].isel(y=32).mean('x').plot()
+        plt.title(f'NG-Aqua {long_name} at Equator vs Time')
+        plt.show()
+
+
+def plot_2d_vars():
+    for var in [
+        'Prec',
+        # 'LHF',
+        # 'SHF',
+        # 'SOLIN',
+        # 'SST',
+    ]:
+        plot_2d(var)
+
+
+def plot_3d_vars():
+    for var in [
+        'FQT',
+        'FSLI',
+        'FU'
+        # 'QT',
+        # 'SLI',
+        # 'U',
+        # 'V',
+        # 'W'
+    ]:
+        ds[var].isel(time=0).dot(ds.layer_mass).plot()
+        plt.title(f'NG-Aqua Column-Integrated {var} at Time = 0')
+        plt.show()
+        ds[var].isel(time=0).isel(y=32).plot()
+        plt.title(f'NG-Aqua Vertical Slice of {var} at Equator (Time=0)')
+        plt.show()
+
+
 if __name__ == '__main__':
-    model = torch.load(dir_ + 'base_model.pkl')
-    plot_net_precip(model, ds)
+    base_model = torch.load(dir_ + 'base_model.pkl')
+    stochastic_model = torch.load(dir_ + 'stochastic_model.pkl')
+    # plot_net_precip(model, ds)
     # plot_net_heating(model, ds)
+    # plot_2d_vars()
+    # plot_2d_over_time()
+    plot_net_precip(stochastic_model, base_model, ds)
+    # plot_3d_vars()