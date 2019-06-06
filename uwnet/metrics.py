c90560adc77916273aa701167b11f691ab9a9cd5
Noah D. Brenowitz <nbren12@gmail.com>
2019-06-06 19:16:38 -0400

    Add utility for mapping xarray functions along dimension
    This gives a strange error though when taking the mean of the output of the
    mapping operations. To circumvent this error, I replace the means with sums in
    r2_score.

--- uwnet/metrics.py
+++ uwnet/metrics.py
@@ -11,8 +11,8 @@ def r2_score(truth, pred, mean_dims, dims=None, w=1.0):
         dims = mean_dims
 
     mu = truth.mean(mean_dims)
-    sum_squares_error = ((truth - pred)**2 * w).mean(dims)
-    sum_squares = ((truth - mu)**2 * w).mean(dims)
+    sum_squares_error = ((truth - pred)**2 * w).sum(dims)
+    sum_squares = ((truth - mu)**2 * w).sum(dims)
 
     return 1 - sum_squares_error / sum_squares
 
