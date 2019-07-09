9e571ddddc9c12e76500f734aeb173dcd1d2caa2
Noah D. Brenowitz <nbren12@gmail.com>
2019-07-09 13:43:38 -0700

    In progress: refactor compute_nn

--- heating_dependence_on_lts_moisture.py
+++ heating_dependence_on_lts_moisture.py
@@ -23,6 +23,12 @@ def tropical_data():
     return tropics
 
 
+def compute_nn(model, averages, batch_dims):
+    bins_key = batch_dims[0]
+    avgs_expanded = averages.rename({bins_key: 'time'}).expand_dims(['x', 'y'], [-1, -2])
+    return model.call_with_xr(avgs_expanded).rename({'time': bins_key}).squeeze()
+
+
 def groupby_and_compute_nn(tropics, model, key, bins):
     bins_key = key + '_bins'
     averages = (tropics
@@ -30,11 +36,7 @@ def groupby_and_compute_nn(tropics, model, key, bins):
      .groupby_bins(key, bins=bins)
      .mean('gridcell'))
 
-    # compute NN output
-    # need to rename the extra dimension to "time" for call_with_xr
-    avgs_expanded = averages.rename({bins_key: 'time'}).expand_dims(['x', 'y'], [-1, -2])
-    output = model.call_with_xr(avgs_expanded).rename({'time': bins_key}).squeeze()
-    
+    output = compute_nn(model, averages, batch_dims=[bins_key])
     for key in output:
         NNkey = 'NN' + key
         averages[NNkey] = output[key]
@@ -49,32 +51,35 @@ def plot_line_cmap(arr, lower_val=4, key='path_bins'):
         arr.plot(y='z', hue=key, color=plt.cm.inferno((it.mid + lower_val)/(25+lower_val)), label=label)
     plt.legend()
 
-# output paths
-lts_plot_path = "sensitivity_to_lts.pdf"
-moisture_plot_path = "sensitivity_to_moist.pdf"
-model_path = "../../nn/NNLowerDecayLR/20.pkl"
+    
+if __name__ == '__main__':
+    # output paths
+    lts_plot_path = "sensitivity_to_lts.pdf"
+    moisture_plot_path = "sensitivity_to_moist.pdf"
+    model_path = "../../nn/NNLowerDecayLR/20.pkl"
 
-# bins for moisture and lower tropospheric stability
-moisture_bins = np.r_[:28:2.5]
-lts_bins = np.r_[2:20:1]
+    # bins for moisture and lower tropospheric stability
+    moisture_bins = np.r_[:28:2.5]
+    lts_bins = np.r_[2:20:1]
 
-# load data
-tropics = tropical_data()
-model = torch.load(model_path)
+    # load data
+    tropics = tropical_data()
+    model = torch.load(model_path)
+    
+    
+    # Lower tropospheric stability plots
+    output_lts_binned = groupby_and_compute_nn(tropics, model=model, key='lts', bins=lts_bins)
+    plt.subplot(121)
+    plot_line_cmap(output_lts_binned.NNQT, key='lts_bins')
+    plt.subplot(122)
+    plot_line_cmap(output_lts_binned.NNSLI, key='lts_bins')
+    plt.savefig(lts_plot_path)
 
-# Lower tropospheric stability plots
-output_lts_binned = groupby_and_compute_nn(tropics, model=model, key='lts', bins=lts_bins)
-plt.subplot(121)
-plot_line_cmap(output_lts_binned.NNQT, key='lts_bins')
-plt.subplot(122)
-plot_line_cmap(output_lts_binned.NNSLI, key='lts_bins')
-plt.savefig(lts_plot_path)
-
-# Mid-tropospheric moisture plots
-plt.figure()
-output = groupby_and_compute_nn(tropics, model, 'path', moisture_bins)
-plt.subplot(121)
-plot_line_cmap(output.NNQT)
-plt.subplot(122)
-plot_line_cmap(output.NNSLI)
-plt.savefig(moisture_plot_path)
+    # Mid-tropospheric moisture plots
+    plt.figure()
+    output = groupby_and_compute_nn(tropics, model, 'path', moisture_bins)
+    plt.subplot(121)
+    plot_line_cmap(output.NNQT)
+    plt.subplot(122)
+    plot_line_cmap(output.NNSLI)
+    plt.savefig(moisture_plot_path)
