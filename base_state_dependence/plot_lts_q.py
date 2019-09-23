9256cb07cc2a28c6c22a562089d40f1b33680aee
Noah D. Brenowitz <nbren12@gmail.com>
2019-09-23 13:14:27 -0700

    Fix import and name errors in plot_lts_q script
    For some reason these script seemed out of date.

--- base_state_dependence/plot_lts_q.py
+++ base_state_dependence/plot_lts_q.py
@@ -1,9 +1,11 @@
-from heating_dependence_on_lts import open_data_with_lts_and_path
+from heating_dependence_on_lts_moisture import open_data_with_lts_and_path
+import matplotlib.pyplot as plt
 
 
 ds = open_data_with_lts_and_path()
 ds.lts[0].plot()
 plt.savefig("lts.png")
 
-ds.q[0].plot()
+plt.figure()
+ds.path[0].plot()
 plt.savefig("q.png")
