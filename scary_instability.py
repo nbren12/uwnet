cc5e547d1de7b5fe3eafd4c44e6216600f721920
Noah D. Brenowitz <nbren12@gmail.com>
2019-11-19 14:26:28 -0800

    Plot scary and standing instability with one function

--- scary_instability.py
+++
@@ -1,9 +0,0 @@
-from uwnet.spectra import plot_structure_from_path
-
-k = .00001
-fig = plot_structure_from_path(
-    "../../nn/NNAll/20.pkl",
-    structure=(1e-6, 50, 1.0),
-)
-
-fig.savefig("scary_instability.pdf")
