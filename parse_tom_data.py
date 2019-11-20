f73843c7b59890d37fe704f222db36b7ae25f181
Noah D. Brenowitz <nbren12@gmail.com>
2019-11-19 17:28:20 -0800

    Parse tom data to netcdf files

--- parse_tom_data.py
+++ parse_tom_data.py
@@ -1,35 +1,80 @@
 import os
 import pickle
 
+import numpy as np
 import xarray as xr
+
+SECONDS_PER_DAY = 86400
+KG_KG_TO_G_KG = 1000
+PATH_PKL = "data/PKL_DATA/"
+NN_NAME = "STAB"
+TRUTH_NAME = "truth"
 
 
 def midpoint(x):
     return (x[1:] + x[:-1]) / 2
 
 
-pathPKL = "data/PKL_DATA/"
-NNname = "STAB"
-truth_name = 'truth'
-filename = "9_30_Fig2_" + NNname + ".pkl"
-path = os.path.join(pathPKL, filename)
+def read_2d_data():
+    filename = "9_30_Fig2_" + NN_NAME + ".pkl"
+    path = os.path.join(PATH_PKL, filename)
 
-with open(path, "rb") as hf:
-    S = pickle.load(hf)
+    with open(path, "rb") as hf:
+        S = pickle.load(hf)
 
-# Coordinates
-coords = {"path_bins": midpoint(S["QMspace"]), "lts_bins": midpoint(S["LTSspace"])}
+    # Coordinates
+    coords = {"path_bins": midpoint(S["QMspace"]), "lts_bins": midpoint(S["LTSspace"])}
 
-data_vars = {}
-# Histogram quantities (Figure 2)
-dims_hist = ['path_bins', 'lts_bins']
-data_vars['net_precipitation_nn'] = (dims_hist, S['PREChist'][NNname])
-data_vars['net_precipitation_src'] = (dims_hist, S['PREChist'][truth_name])
-data_vars['net_heating_nn'] = (dims_hist, S['HEAThist'][NNname])
-data_vars['net_heating_src'] = (dims_hist, S['HEAThist'][truth_name])
-data_vars['count'] = (dims_hist, S['Whist'])
+    data_vars = {}
+    # Histogram quantities (Figure 2)
+    dims_hist = ["path_bins", "lts_bins"]
+    data_vars["net_precipitation_nn"] = (dims_hist, S["PREChist"][NN_NAME])
+    data_vars["net_precipitation_src"] = (dims_hist, S["PREChist"][TRUTH_NAME])
+    data_vars["net_heating_nn"] = (dims_hist, S["HEAThist"][NN_NAME])
+    data_vars["net_heating_src"] = (dims_hist, S["HEAThist"][TRUTH_NAME])
+    data_vars["count"] = (dims_hist, S["Whist"])
 
-# Vertical quantities
+    # Vertical quantities Figure 3 and 4
 
-output_dataset = xr.Dataset(data_vars, coords=coords)
+    return xr.Dataset(data_vars, coords=coords)
+
+
+def read_3d_data():
+
+    filename = "9_30_Fig3_" + NN_NAME + ".pkl"
+    path_to_vertical_data = os.path.join(PATH_PKL, filename)
+    path_to_coords = os.path.join(PATH_PKL, "11_2_SPCAM_coordinates.pkl")
+
+    with open(path_to_coords, "rb") as f:
+        Scoor = pickle.load(f)
+
+    with open(path_to_vertical_data, "rb") as f:
+        S34 = pickle.load(f)
+
+    # build dataset
+    data_vars = {}
+    dims_3d = ["path_bins", "lts_bins", "z"]
+
+    data_vars["Q1NN"] = (dims_3d, S34["dTdthist"][NN_NAME] * SECONDS_PER_DAY)
+    data_vars["Q2NN"] = (dims_3d, S34["dqdthist"][NN_NAME] * SECONDS_PER_DAY)
+
+    data_vars["Q1"] = (
+        dims_3d,
+        S34["dTdthist"][TRUTH_NAME] * SECONDS_PER_DAY * KG_KG_TO_G_KG,
+    )
+    data_vars["Q2"] = (
+        dims_3d,
+        S34["dqdthist"][TRUTH_NAME] * SECONDS_PER_DAY * KG_KG_TO_G_KG,
+    )
+
+    data_vars["QV"] = (dims_3d, S34["QVhist"] * SECONDS_PER_DAY * KG_KG_TO_G_KG)
+    data_vars["T"] = (dims_3d, S34["Thist"])
+
+    # TODO Ask tom if scoor['lev'] as actually the pressure
+    data_vars["p"] = (["z"], np.asarray(Scoor["lev"]))
+
+    return xr.Dataset(data_vars)
+
+
+output_dataset = xr.merge([read_3d_data(), read_2d_data()])
 output_dataset.to_netcdf("data/tom_binned.nc")
