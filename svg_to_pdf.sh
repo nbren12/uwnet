31506561831a9d844a8df2841f1cf54c1283be31
Noah D. Brenowitz <nbren12@gmail.com>
2020-08-18 12:34:38 -0700

    Repair svg_to_pdf.sh script to inkscape v1.0
    This version changed the command line interface for exporting pdfs

--- svg_to_pdf.sh
+++ svg_to_pdf.sh
@@ -4,6 +4,6 @@ svgFiles=$(find . -name '*.svg')
 for f in $svgFiles
 do
     base=${f%.*}
-    inkscape $base.svg --batch-process --export-type=pdf --export-filename=$base.pdf > /dev/null
+    inkscape $base.svg --batch-process --export-type=pdf --export-filename=$base.pdf 2> /dev/null
 done
 
